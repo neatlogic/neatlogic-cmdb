4bcff986ff9aec592e3e5730f80b6af40818c942
chenqiwei <chenqw@techsure.com.cn>
2020-11-12 15:18:00 +0800

    bugfixed

--- src/main/java/codedriver/module/cmdb/api/citype/SaveCiTypeApi.java
+++ src/main/java/codedriver/module/cmdb/api/citype/SaveCiTypeApi.java
@@ -10,7 +10,6 @@ import codedriver.framework.reminder.core.OperationTypeEnum;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
 import codedriver.framework.restful.annotation.OperationType;
-import codedriver.framework.restful.annotation.Output;
 import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import codedriver.module.cmdb.dao.mapper.ci.CiTypeMapper;
