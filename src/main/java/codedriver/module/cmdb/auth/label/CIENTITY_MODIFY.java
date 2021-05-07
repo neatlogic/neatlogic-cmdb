355a115f8a6fd34ca084c4ad60665e3cfcb0c602
陈其炜 <chenqw@techsure.com.cn>
2021-05-07 10:59:15 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/module/cmdb/auth/label/CIENTITY_MODIFY.java
+++ src/main/java/codedriver/module/cmdb/auth/label/CIENTITY_MODIFY.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
 package codedriver.module.cmdb.auth.label;
 
 import codedriver.framework.auth.core.AuthBase;
@@ -20,7 +25,7 @@ public class CIENTITY_MODIFY extends AuthBase {
     }
 
     @Override
-    public Integer sort() {
+    public Integer getSort() {
         return 4;
     }
 }
