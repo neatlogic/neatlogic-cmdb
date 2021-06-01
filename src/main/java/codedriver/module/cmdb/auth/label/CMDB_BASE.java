a27f09cc7e3440630b5f5419cecd6fe005167e03
林邦泉 <linbq@techsure.com.cn>
2021-06-01 10:49:55 +0800

    --story=1004770 --user=林邦泉 组合工具需要访问资源中心数据的相关接口 https://www.tapd.cn/54247054/s/1157578

--- src/main/java/codedriver/module/cmdb/auth/label/CMDB_BASE.java
+++ src/main/java/codedriver/module/cmdb/auth/label/CMDB_BASE.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
 package codedriver.module.cmdb.auth.label;
 
 import codedriver.framework.auth.core.AuthBase;
@@ -11,7 +16,7 @@ public class CMDB_BASE extends AuthBase {
 
 	@Override
 	public String getAuthIntroduction() {
-		return "查看配置";
+		return "拥有此权限才能使用配置管理功能";
 	}
 
 	@Override
--- src/main/java/codedriver/module/cmdb/auth/label/RELTYPE_MODIFY.java
+++ src/main/java/codedriver/module/cmdb/auth/label/RELTYPE_MODIFY.java
@@ -1,6 +1,14 @@
+/*
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
 package codedriver.module.cmdb.auth.label;
 
 import codedriver.framework.auth.core.AuthBase;
+
+import java.util.Collections;
+import java.util.List;
 
 public class RELTYPE_MODIFY extends AuthBase {
 
@@ -22,5 +30,10 @@ public class RELTYPE_MODIFY extends AuthBase {
 	@Override
 	public Integer getSort() {
 		return 6;
+	}
+
+	@Override
+	public List<Class<? extends AuthBase>> getIncludeAuths() {
+		return Collections.singletonList(CMDB_BASE.class);
 	}
 }
