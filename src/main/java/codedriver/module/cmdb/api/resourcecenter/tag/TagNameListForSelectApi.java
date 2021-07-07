8ec68dd34fb66248a127e5241b62c52d362c2aef
林邦泉 <linbq@techsure.com.cn>
2021-07-07 11:04:18 +0800

    --task=1004912 --user=林邦泉 单个资产设置标签接口 https://www.tapd.cn/54247054/s/1162890

--- src/main/java/codedriver/module/cmdb/api/resourcecenter/tag/TagNameListForSelectApi.java
+++ src/main/java/codedriver/module/cmdb/api/resourcecenter/tag/TagNameListForSelectApi.java
@@ -9,6 +9,7 @@ import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.cmdb.dto.tag.TagVo;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.common.dto.BasePageVo;
+import codedriver.framework.common.dto.ValueTextVo;
 import codedriver.framework.common.util.PageUtil;
 import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
@@ -16,7 +17,9 @@ import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import codedriver.module.cmdb.auth.label.CMDB_BASE;
 import codedriver.module.cmdb.dao.mapper.resourcecenter.ResourceCenterMapper;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -24,27 +27,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 查询资源中心标签列表（下拉列表专用）接口
- *
  * @author linbq
- * @since 2021/5/30 14:42
- **/
-@Service
+ * @since 2021/7/7 10:15
+ **/@Service
 @AuthAction(action = CMDB_BASE.class)
 @OperationType(type = OperationTypeEnum.SEARCH)
-public class TagListForSelectApi extends PrivateApiComponentBase {
+public class TagNameListForSelectApi extends PrivateApiComponentBase {
 
     @Resource
     private ResourceCenterMapper resourceCenterMapper;
 
     @Override
     public String getToken() {
-        return "resourcecenter/tag/list/forselect";
+        return "resourcecenter/tag/name/list/forselect";
     }
 
     @Override
     public String getName() {
-        return "查询资源中心标签列表（下拉列表专用）";
+        return "查询资源中心标签名称列表（下拉列表专用）";
     }
 
     @Override
@@ -54,34 +54,44 @@ public class TagListForSelectApi extends PrivateApiComponentBase {
 
     @Input({
             @Param(name = "keyword", type = ApiParamType.STRING, desc = "模糊搜索"),
-            @Param(name = "defaultValue", type = ApiParamType.JSONARRAY, desc = "用于回显的标签ID列表"),
+            @Param(name = "defaultValue", type = ApiParamType.JSONARRAY, desc = "用于回显的标签列表"),
             @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "当前页"),
             @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "每页数据条目"),
             @Param(name = "needPage", type = ApiParamType.BOOLEAN, desc = "是否需要分页，默认true")
     })
     @Output({
             @Param(explode = BasePageVo.class),
-            @Param(name = "tbodyList", explode = TagVo[].class, desc = "标签列表")
+            @Param(name = "tbodyList", explode = ValueTextVo[].class, desc = "标签列表")
     })
-    @Description(desc = "查询资源中心标签列表（下拉列表专用）")
+    @Description(desc = "查询资源中心标签名称列表（下拉列表专用）")
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
+        List<ValueTextVo> tbodyList = new ArrayList<>();
         JSONObject resultObj = new JSONObject();
-        int pageCount = 0;
+        resultObj.put("tbodyList", tbodyList);
+        JSONArray defaultValue = paramObj.getJSONArray("defaultValue");
+        if (CollectionUtils.isNotEmpty(defaultValue)) {
+            for (int i = 0; i < defaultValue.size(); i++) {
+                String tagName = defaultValue.getString(i);
+                tbodyList.add(new ValueTextVo(tagName, tagName));
+            }
+            return resultObj;
+        }
         TagVo searchVo = JSON.toJavaObject(paramObj, TagVo.class);
         int rowNum = resourceCenterMapper.getTagCount(searchVo);
         if (rowNum > 0) {
-            pageCount = PageUtil.getPageCount(rowNum, searchVo.getPageSize());
-            List<TagVo> tagVoList = resourceCenterMapper.getTagListForSelect(searchVo);
-            resultObj.put("tbodyList", tagVoList);
-        } else {
-            resultObj.put("tbodyList", new ArrayList<>());
+            searchVo.setRowNum(rowNum);
+            List<String> tagNameList = resourceCenterMapper.getTagNameListForSelect(searchVo);
+            if (CollectionUtils.isNotEmpty(tagNameList)) {
+                for (String tagName : tagNameList) {
+                    tbodyList.add(new ValueTextVo(tagName, tagName));
+                }
+            }
         }
         resultObj.put("rowNum", rowNum);
-        resultObj.put("pageCount", pageCount);
+        resultObj.put("pageCount", searchVo.getPageCount());
         resultObj.put("currentPage", searchVo.getCurrentPage());
         resultObj.put("pageSize", searchVo.getPageSize());
         return resultObj;
     }
-
 }
