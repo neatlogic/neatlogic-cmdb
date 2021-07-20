cd9caeaa26e5cb61769c184e25cc67ee19c79f1b
林邦泉 <linbq@techsure.com.cn>
2021-07-20 20:23:43 +0800

    --task=1005037 --user=林邦泉 后端-流程图数据结构调整 https://www.tapd.cn/54247054/s/1164479

--- src/main/java/codedriver/module/cmdb/process/stephandler/CIEntitySyncProcessUtilHandler.java
+++ src/main/java/codedriver/module/cmdb/process/stephandler/CIEntitySyncProcessUtilHandler.java
@@ -2,6 +2,7 @@ package codedriver.module.cmdb.process.stephandler;
 
 import java.util.*;
 
+import codedriver.framework.process.dto.processconfig.ActionConfigActionVo;
 import codedriver.framework.process.dto.processconfig.ActionConfigVo;
 import codedriver.framework.process.dto.processconfig.NotifyPolicyConfigVo;
 import codedriver.framework.process.util.ProcessConfigUtil;
@@ -47,21 +48,27 @@ public class CIEntitySyncProcessUtilHandler extends ProcessStepInternalHandlerBa
     public void makeupProcessStep(ProcessStepVo processStepVo, JSONObject stepConfigObj) {
         /** 组装通知策略id **/
         JSONObject notifyPolicyConfig = stepConfigObj.getJSONObject("notifyPolicyConfig");
-        if (MapUtils.isNotEmpty(notifyPolicyConfig)) {
-            Long policyId = notifyPolicyConfig.getLong("policyId");
+        NotifyPolicyConfigVo notifyPolicyConfigVo = JSONObject.toJavaObject(notifyPolicyConfig, NotifyPolicyConfigVo.class);
+        if (notifyPolicyConfigVo != null) {
+            Long policyId = notifyPolicyConfigVo.getPolicyId();
             if (policyId != null) {
                 processStepVo.setNotifyPolicyId(policyId);
             }
         }
 
-        JSONArray actionList = (JSONArray) JSONPath.read(stepConfigObj.toJSONString(), "actionConfig.actionList");
-        if(CollectionUtils.isNotEmpty(actionList)){
-            for (int i = 0; i < actionList.size(); i++) {
-                JSONObject ationObj = actionList.getJSONObject(i);
-                String integrationUuid = ationObj.getString("integrationUuid");
-                if(StringUtils.isNotBlank(integrationUuid)) {
-                    processStepVo.getIntegrationUuidList().add(integrationUuid);
+        JSONObject actionConfig = stepConfigObj.getJSONObject("actionConfig");
+        ActionConfigVo actionConfigVo = JSONObject.toJavaObject(actionConfig, ActionConfigVo.class);
+        if (actionConfigVo != null) {
+            List<ActionConfigActionVo> actionList = actionConfigVo.getActionList();
+            if (CollectionUtils.isNotEmpty(actionList)) {
+                List<String> integrationUuidList = new ArrayList<>();
+                for (ActionConfigActionVo actionVo : actionList) {
+                    String integrationUuid = actionVo.getIntegrationUuid();
+                    if (StringUtils.isNotBlank(integrationUuid)) {
+                        integrationUuidList.add(integrationUuid);
+                    }
                 }
+                processStepVo.setIntegrationUuidList(integrationUuidList);
             }
         }
 
@@ -86,10 +93,10 @@ public class CIEntitySyncProcessUtilHandler extends ProcessStepInternalHandlerBa
                 }
                 processStepVo.setWorkerPolicyList(workerPolicyList);
             }
-            //保存回复模版ID
-            Long commentTemplateId = workerPolicyConfig.getLong("commentTemplateId");
-            processStepVo.setCommentTemplateId(commentTemplateId);
         }
+        //保存回复模版ID
+        Long commentTemplateId = stepConfigObj.getLong("commentTemplateId");
+        processStepVo.setCommentTemplateId(commentTemplateId);
         /**
          * FIXME 设置CMDB节点设置
          */
