e39faec4fc18b8f78cd1d5b61b6c2fba8dede0b9
林邦泉 <linbq@techsure.com.cn>
2021-10-20 16:58:06 +0800

    --bug=1008746 --user=林邦泉 应用清单-修改页面显示条数无效 https://www.tapd.cn/54247054/s/1184063

--- src/main/java/codedriver/module/cmdb/api/resourcecenter/appmodule/AppModuleListApi.java
+++ src/main/java/codedriver/module/cmdb/api/resourcecenter/appmodule/AppModuleListApi.java
@@ -11,19 +11,20 @@ import codedriver.framework.cmdb.dto.resourcecenter.ResourceSearchVo;
 import codedriver.framework.cmdb.dto.resourcecenter.ResourceVo;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.common.dto.BasePageVo;
-import codedriver.framework.dto.UserVo;
 import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import codedriver.framework.util.TableResultUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author linbq
@@ -64,33 +65,24 @@ public class AppModuleListApi extends PrivateApiComponentBase {
     @Description(desc = "查询资源中应用模块列表")
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
-        JSONObject resultObj = new JSONObject();
-        List<ResourceVo> resourceVoList = null;
+        List<ResourceVo> resourceVoList = new ArrayList<>();
         ResourceSearchVo searchVo = JSON.toJavaObject(paramObj, ResourceSearchVo.class);
-        int rowNum = resourceCenterMapper.getAppModuleCount(searchVo);
-        if (rowNum > 0) {
+        Set<Long> idSet = resourceCenterMapper.getAppModuleIdList(searchVo);
+        if (CollectionUtils.isNotEmpty(idSet)) {
+            int rowNum = idSet.size();
             searchVo.setRowNum(rowNum);
-            List<Long> idList = resourceCenterMapper.getAppModuleIdList(searchVo);
-            if (CollectionUtils.isNotEmpty(idList)) {
-                resourceVoList = resourceCenterMapper.getAppModuleListByIdList(idList, TenantContext.get().getDataDbName());
-//                for (ResourceVo resourceVo : resourceVoList) {
-//                    if (StringUtils.isNotBlank(resourceVo.getFcu())) {
-//                        resourceVo.setFcuVo(new UserVo(resourceVo.getFcu()));
-//                    }
-//                    if (StringUtils.isNotBlank(resourceVo.getLcu())) {
-//                        resourceVo.setLcuVo(new UserVo(resourceVo.getLcu()));
-//                    }
-//                }
+            if (searchVo.getCurrentPage() <= searchVo.getPageCount()) {
+                int fromIndex = searchVo.getStartNum();
+                int toIndex = fromIndex + searchVo.getPageSize();
+                toIndex = toIndex >  rowNum ? rowNum : toIndex;
+                List<Long> idList = new ArrayList<>(idSet);
+                idList.sort(Comparator.reverseOrder());
+                List<Long> currentPageIdList = idList.subList(fromIndex, toIndex);
+                if (CollectionUtils.isNotEmpty(currentPageIdList)) {
+                    resourceVoList = resourceCenterMapper.getAppModuleListByIdList(currentPageIdList, TenantContext.get().getDataDbName());
+                }
             }
         }
-        if (resourceVoList == null) {
-            resourceVoList = new ArrayList<>();
-        }
-        resultObj.put("tbodyList", resourceVoList);
-        resultObj.put("rowNum", rowNum);
-        resultObj.put("pageCount", searchVo.getPageCount());
-        resultObj.put("currentPage", searchVo.getCurrentPage());
-        resultObj.put("pageSize", searchVo.getPageSize());
-        return resultObj;
+        return TableResultUtil.getResult(resourceVoList, searchVo);
     }
 }
