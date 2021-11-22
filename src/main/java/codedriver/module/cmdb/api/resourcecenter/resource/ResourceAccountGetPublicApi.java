3ab9192ec39a4805243b45ae2d57bf23ba1c3cb2
lvzk <lvzk@techsure.com.cn>
2021-11-22 20:50:59 +0800

    update autoexec 获取密码借口

--- src/main/java/codedriver/module/cmdb/api/resourcecenter/resource/ResourceAccountGetPublicApi.java
+++ src/main/java/codedriver/module/cmdb/api/resourcecenter/resource/ResourceAccountGetPublicApi.java
@@ -5,15 +5,19 @@
 
 package codedriver.module.cmdb.api.resourcecenter.resource;
 
+import codedriver.framework.autoexec.dto.job.AutoexecJobVo;
 import codedriver.framework.cmdb.dao.mapper.resourcecenter.ResourceCenterMapper;
 import codedriver.framework.cmdb.dto.resourcecenter.AccountVo;
+import codedriver.framework.cmdb.exception.resourcecenter.ResourceCenterAccountNotFoundException;
 import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.common.util.RC4Util;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
 import codedriver.framework.restful.annotation.Output;
 import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.core.publicapi.PublicApiComponentBase;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -52,6 +56,10 @@ public class ResourceAccountGetPublicApi extends PublicApiComponentBase {
     @Description(desc="根据资产id、账号名和协议获取账号密码")
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
-        return resourceCenterMapper.getResourceAccountByResourceIdAndProtocolAndProtocolPortAndAccount(paramObj.getLong("resourceId"),paramObj.getString("protocol"),paramObj.getInteger("protocolPort"),paramObj.getString("username"));
+        AccountVo accountVo = resourceCenterMapper.getResourceAccountByResourceIdAndProtocolAndProtocolPortAndAccount(paramObj.getLong("resourceId"),paramObj.getString("protocol"),paramObj.getInteger("protocolPort"),paramObj.getString("username"));
+        if(accountVo == null){
+            throw new ResourceCenterAccountNotFoundException();
+        }
+        return "{ENCRYPTED}"+RC4Util.encrypt(AutoexecJobVo.AUTOEXEC_RC4_KEY,RC4Util.decrypt(accountVo.getPasswordCipher().replace("RC4:", StringUtils.EMPTY)));
     }
 }
