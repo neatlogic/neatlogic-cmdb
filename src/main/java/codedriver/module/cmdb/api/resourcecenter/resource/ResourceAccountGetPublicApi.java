dd1ae7607ea9e4ff0cedda50206b1810d2115dc1
lvzk <lvzk@techsure.com.cn>
2021-11-22 19:50:12 +0800

    tagent 注册逻辑优化

---
+++ src/main/java/codedriver/module/cmdb/api/resourcecenter/resource/ResourceAccountGetPublicApi.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c)  2021 TechSure Co.,Ltd.  All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.module.cmdb.api.resourcecenter.resource;
+
+import codedriver.framework.cmdb.dao.mapper.resourcecenter.ResourceCenterMapper;
+import codedriver.framework.cmdb.dto.resourcecenter.AccountVo;
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.restful.annotation.Description;
+import codedriver.framework.restful.annotation.Input;
+import codedriver.framework.restful.annotation.Output;
+import codedriver.framework.restful.annotation.Param;
+import codedriver.framework.restful.core.publicapi.PublicApiComponentBase;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class ResourceAccountGetPublicApi extends PublicApiComponentBase {
+    @Resource
+    ResourceCenterMapper resourceCenterMapper;
+
+    @Override
+    public String getName() {
+        return "获取账号密码";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Override
+    public String getToken() {
+        return "resourcecenter/resource/account/get";
+    }
+
+    @Input({
+            @Param(name = "resourceId", type = ApiParamType.LONG, isRequired = true, desc = "资产id"),
+            @Param(name = "host", type = ApiParamType.STRING, xss = true, desc = "host ip"),
+            @Param(name = "port", type = ApiParamType.INTEGER, desc = "端口"),
+            @Param(name = "protocol", type = ApiParamType.STRING, isRequired = true, desc = "协议"),
+            @Param(name = "protocolPort", type = ApiParamType.INTEGER, isRequired = true, desc = "协议端口"),
+            @Param(name = "username", type = ApiParamType.STRING, isRequired = true, desc = "账号名")
+    })
+    @Output({
+            @Param(name = "name", explode = AccountVo[].class, desc = "账号"),
+    })
+    @Description(desc="根据资产id、账号名和协议获取账号密码")
+    @Override
+    public Object myDoService(JSONObject paramObj) throws Exception {
+        return resourceCenterMapper.getResourceAccountByResourceIdAndProtocolAndProtocolPortAndAccount(paramObj.getLong("resourceId"),paramObj.getString("protocol"),paramObj.getInteger("protocolPort"),paramObj.getString("username"));
+    }
+}
