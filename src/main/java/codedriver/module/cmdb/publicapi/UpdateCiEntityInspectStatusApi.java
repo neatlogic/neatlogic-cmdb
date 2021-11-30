db214315b9726f88874eb320faf95e825e4ce12c
陈其炜 <chenqw@techsure.com.cn>
2021-11-30 14:03:42 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/module/cmdb/publicapi/UpdateCiEntityInspectStatusApi.java
+++ src/main/java/codedriver/module/cmdb/publicapi/UpdateCiEntityInspectStatusApi.java
@@ -43,7 +43,7 @@ public class UpdateCiEntityInspectStatusApi extends PublicApiComponentBase {
     }
 
     @Input({@Param(name = "ciEntityId", type = ApiParamType.LONG, isRequired = true, desc = "配置项id"),
-            @Param(name = "inspectStatus", type = ApiParamType.ENUM, rule = "normal,warning,critical", isRequired = true, desc = "巡检状态"),
+            @Param(name = "inspectStatus", type = ApiParamType.ENUM, rule = "normal,warn,critical,fatal", isRequired = true, desc = "巡检状态"),
             @Param(name = "inspectTime", type = ApiParamType.LONG, isRequired = true, desc = "巡检时间，格式：距1970年1月1日0时0分0秒的豪秒数")})
     @Output({})
     @Description(desc = "修改配置项巡检状态接口")
