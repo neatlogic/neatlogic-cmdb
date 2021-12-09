e0cf881d214518ba33d6f7cf7c35b686fbf832dd
longrf <techsure901>
2021-12-09 14:39:41 +0800

    --story=1005638 --user=龙儒锋 资源中心-资产清单添加脚本管理 https://www.tapd.cn/54247054/s/1193728

--- src/main/java/codedriver/module/cmdb/api/resourcecenter/resource/ResourceScriptSaveApi.java
+++ src/main/java/codedriver/module/cmdb/api/resourcecenter/resource/ResourceScriptSaveApi.java
@@ -1,12 +1,10 @@
 package codedriver.module.cmdb.api.resourcecenter.resource;
 
-import codedriver.framework.asynchronization.threadlocal.TenantContext;
 import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.autoexec.dao.mapper.AutoexecScriptMapper;
 import codedriver.framework.autoexec.dto.script.AutoexecScriptVo;
 import codedriver.framework.autoexec.exception.AutoexecScriptNotFoundException;
 import codedriver.framework.cmdb.dao.mapper.resourcecenter.ResourceCenterMapper;
-import codedriver.framework.cmdb.exception.resourcecenter.ResourceNotFoundException;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
@@ -53,7 +51,8 @@ public class ResourceScriptSaveApi extends PrivateApiComponentBase {
 
     @Input({
             @Param(name = "resourceId", type = ApiParamType.LONG, isRequired = true, desc = "资源id"),
-                @Param(name = "scriptIdList", type = ApiParamType.JSONARRAY, desc = "脚本列表（自定义工具）")
+            @Param(name = "scriptIdList", type = ApiParamType.JSONARRAY, desc = "脚本列表（自定义工具）"),
+            @Param(name = "urlSequence", type = ApiParamType.STRING, desc = "url序列")
     })
     @Description(desc = "保存资源脚本")
     @Override
@@ -61,22 +60,23 @@ public class ResourceScriptSaveApi extends PrivateApiComponentBase {
 
         JSONArray scriptIdArray = paramObj.getJSONArray("scriptIdList");
         Long resourceId = paramObj.getLong("resourceId");
+        String urlSequence = paramObj.getString("urlSequence");
         resourceCenterMapper.deleteResourceScriptByResourceId(resourceId);
         if (CollectionUtils.isEmpty(scriptIdArray)) {
             return null;
         }
         List<Long> scriptIdList = scriptIdArray.toJavaList(Long.class);
-        String schemaName = TenantContext.get().getDataDbName();
+/*        String schemaName = TenantContext.get().getDataDbName();
         if (resourceCenterMapper.checkResourceIsExists(resourceId, schemaName) == 0) {
             throw new ResourceNotFoundException(resourceId);
-        }
+        }*/
         for (Long scriptId : scriptIdList) {
             AutoexecScriptVo script = autoexecScriptMapper.getScriptBaseInfoById(scriptId);
             if (script == null) {
                 throw new AutoexecScriptNotFoundException(scriptId);
             }
         }
-        resourceCenterMapper.insertResourceScriptIdList(resourceId, scriptIdList);
+        resourceCenterMapper.insertResourceScriptIdList(resourceId, scriptIdList,urlSequence);
 
         return null;
     }
