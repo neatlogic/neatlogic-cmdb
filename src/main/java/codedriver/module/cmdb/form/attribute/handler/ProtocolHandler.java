ed8bc301adc71c85e556ce715d19d1fb49a95bd3
林邦泉 <linbq@techsure.com.cn>
2021-12-27 11:40:57 +0800

    --task=1005735 --user=林邦泉 后端-新增表单组件1.连接协议 2.执行目标 https://www.tapd.cn/54247054/s/1197829

---
+++ src/main/java/codedriver/module/cmdb/form/attribute/handler/ProtocolHandler.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright(c) 2021 TechSureCo.,Ltd.AllRightsReserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.module.cmdb.form.attribute.handler;
+
+import codedriver.framework.common.constvalue.ParamType;
+import codedriver.framework.form.attribute.core.FormHandlerBase;
+import codedriver.framework.form.constvalue.FormConditionModel;
+import codedriver.framework.form.dto.AttributeDataVo;
+import codedriver.framework.form.exception.AttributeValidException;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author linbq
+ * @since 2021/12/27 11:18
+ **/
+@Component
+public class ProtocolHandler extends FormHandlerBase {
+    @Override
+    public String getHandler() {
+        return "protocol";
+    }
+
+    @Override
+    public String getHandlerName() {
+        return "连接协议";
+    }
+
+    @Override
+    public String getHandlerType(FormConditionModel model) {
+        return null;
+    }
+
+    @Override
+    public String getIcon() {
+        return "tsfont-zirenwu";
+    }
+
+    @Override
+    public ParamType getParamType() {
+        return ParamType.ARRAY;
+    }
+
+    @Override
+    public String getDataType() {
+        return "list";
+    }
+
+    @Override
+    public boolean isAudit() {
+        return true;
+    }
+
+    @Override
+    public boolean isConditionable() {
+        return true;
+    }
+
+    @Override
+    public boolean isShowable() {
+        return true;
+    }
+
+    @Override
+    public boolean isValueable() {
+        return true;
+    }
+
+    @Override
+    public boolean isFilterable() {
+        return true;
+    }
+
+    @Override
+    public boolean isExtendable() {
+        return false;
+    }
+
+    @Override
+    public boolean isForTemplate() {
+        return true;
+    }
+
+    @Override
+    public String getModule() {
+        return "cmdb";
+    }
+
+    @Override
+    public JSONObject valid(AttributeDataVo attributeDataVo, JSONObject configObj) throws AttributeValidException {
+        return null;
+    }
+
+    @Override
+    public Object valueConversionText(AttributeDataVo attributeDataVo, JSONObject configObj) {
+        return "已更新";
+    }
+
+    @Override
+    public Object dataTransformationForEmail(AttributeDataVo attributeDataVo, JSONObject configObj) {
+        return null;
+    }
+
+    @Override
+    public Object textConversionValue(List<String> values, JSONObject config) {
+        return null;
+    }
+
+    @Override
+    public int getSort() {
+        return 22;
+    }
+}
