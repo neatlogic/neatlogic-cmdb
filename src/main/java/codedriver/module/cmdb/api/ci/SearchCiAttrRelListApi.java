37dfa9c01c23aab560e8899ce2b6cc42bdd34dbc
陈其炜 <chenqw@techsure.com.cn>
2022-01-04 21:15:52 +0800

    bugfixed
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/module/cmdb/api/ci/SearchCiAttrRelListApi.java
+++ src/main/java/codedriver/module/cmdb/api/ci/SearchCiAttrRelListApi.java
@@ -1,5 +1,5 @@
 /*
- * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
@@ -24,6 +24,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 @Service
@@ -65,27 +66,32 @@ public class SearchCiAttrRelListApi extends PrivateApiComponentBase {
         List<AttrVo> attrList = attrMapper.getAttrByCiId(ciId);
         List<RelVo> relList = RelUtil.ClearRepeatRel(relMapper.getRelByCiId(ciId));
         if (StringUtils.isNotBlank(keyword)) {
+            keyword = keyword.toLowerCase(Locale.ROOT);
             //因为有二级缓存，数据量也不大，可以直接在应用层过滤关键字实现搜索效果
-            attrList = attrList.stream().filter(d -> d.getName().contains(keyword)).collect(Collectors.toList());
-            relList = relList.stream().filter(d -> (d.getDirection().equals(RelDirectionType.FROM.getValue()) && d.getToName().contains(keyword))
-                    || (d.getDirection().equals(RelDirectionType.TO.getValue()) && d.getFromName().contains(keyword))).collect(Collectors.toList());
+            String finalKeyword = keyword;
+            attrList = attrList.stream().filter(d -> d.getName().toLowerCase(Locale.ROOT).contains(finalKeyword)).collect(Collectors.toList());
+            relList = relList.stream().filter(d -> (d.getDirection().equals(RelDirectionType.FROM.getValue()) && d.getToName().toLowerCase(Locale.ROOT).contains(finalKeyword))
+                    || (d.getDirection().equals(RelDirectionType.TO.getValue()) && d.getFromName().toLowerCase(Locale.ROOT).contains(finalKeyword))).collect(Collectors.toList());
         }
         for (AttrVo attrVo : attrList) {
             JSONObject attrObj = new JSONObject();
             attrObj.put("id", attrVo.getId());
             attrObj.put("name", attrVo.getName());
             attrObj.put("label", attrVo.getLabel());
+            attrObj.put("targetCiId", attrVo.getTargetCiId());
             returnList.add(attrObj);
         }
         for (RelVo relVo : relList) {
             JSONObject relObj = new JSONObject();
             relObj.put("id", relVo.getId());
             if (relVo.getDirection().equals(RelDirectionType.FROM.getValue())) {
-                relObj.put("name", relVo.getToCiName());
-                relObj.put("label", relVo.getToCiLabel());
+                relObj.put("name", relVo.getToName());
+                relObj.put("label", relVo.getToLabel());
+                relObj.put("targetCiId", relVo.getToCiId());
             } else if (relVo.getDirection().equals(RelDirectionType.TO.getValue())) {
-                relObj.put("name", relVo.getFromCiName());
-                relObj.put("label", relVo.getFromCiLabel());
+                relObj.put("name", relVo.getFromName());
+                relObj.put("label", relVo.getFromLabel());
+                relObj.put("targetCiId", relVo.getFromCiId());
             }
             returnList.add(relObj);
         }
