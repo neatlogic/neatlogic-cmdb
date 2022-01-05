dd15384f29ccbacf6b31f67689c3c9b0c89c3981
longrf <techsure901>
2022-01-05 11:32:10 +0800

    添加接口-根据资源id查找关联的账户

---
+++ src/main/java/codedriver/module/cmdb/api/resourcecenter/resource/ResourceAccountListApi.java
@@ -0,0 +1,63 @@
+package codedriver.module.cmdb.api.resourcecenter.resource;
+
+import codedriver.framework.asynchronization.threadlocal.TenantContext;
+import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.cmdb.dao.mapper.resourcecenter.ResourceCenterMapper;
+import codedriver.framework.cmdb.dto.resourcecenter.AccountVo;
+import codedriver.framework.cmdb.exception.resourcecenter.ResourceNotFoundException;
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.restful.annotation.Input;
+import codedriver.framework.restful.annotation.OperationType;
+import codedriver.framework.restful.annotation.Output;
+import codedriver.framework.restful.annotation.Param;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import codedriver.module.cmdb.auth.label.CMDB_BASE;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @author longrf
+ * @date 2022/1/5 10:51 上午
+ */
+@Service
+@AuthAction(action = CMDB_BASE.class)
+@OperationType(type = OperationTypeEnum.SEARCH)
+public class ResourceAccountListApi extends PrivateApiComponentBase {
+
+    @Resource
+    private ResourceCenterMapper resourceCenterMapper;
+
+    @Override
+    public String getName() {
+        return "根据资源id查询关联的账户信息";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Override
+    public String getToken() {
+        return "resourcecenter/resource/account/list";
+    }
+
+    @Input({
+            @Param(name = "resourceId", type = ApiParamType.LONG, desc = "资源id")
+    })
+    @Output({
+            @Param(name = "tbodyList", explode = AccountVo[].class, desc = "账号列表")})
+    @Override
+    public Object myDoService(JSONObject paramObj) throws Exception {
+        Long resourceId = paramObj.getLong("resourceId");
+        String schemaName = TenantContext.get().getDataDbName();
+        if (resourceCenterMapper.checkResourceIsExists(resourceId, schemaName) == 0) {
+            throw new ResourceNotFoundException(resourceId);
+        }
+        return resourceCenterMapper.getResourceAccountListByResourceId(resourceId);
+    }
+
+}
