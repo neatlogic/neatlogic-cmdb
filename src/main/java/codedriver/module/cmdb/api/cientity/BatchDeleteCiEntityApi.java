b1b94058b4da664acafb8a6224d445a78659cb62
fengt <fengt@techsure.com.cn>
2022-01-21 10:49:58 +0800

    BatchDeleteCiEntityApi.java实现接口IBatchDeleteCiEntityApiCrossoverService

--- src/main/java/codedriver/module/cmdb/api/cientity/BatchDeleteCiEntityApi.java
+++ src/main/java/codedriver/module/cmdb/api/cientity/BatchDeleteCiEntityApi.java
@@ -5,13 +5,27 @@
 
 package codedriver.module.cmdb.api.cientity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
 import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.cmdb.crossover.IBatchDeleteCiEntityApiCrossoverService;
 import codedriver.framework.cmdb.dto.cientity.CiEntityVo;
-import codedriver.framework.cmdb.enums.group.GroupType;
 import codedriver.framework.cmdb.enums.TransactionActionType;
+import codedriver.framework.cmdb.enums.group.GroupType;
 import codedriver.framework.cmdb.exception.cientity.CiEntityAuthException;
 import codedriver.framework.common.constvalue.ApiParamType;
-import codedriver.framework.restful.annotation.*;
+import codedriver.framework.restful.annotation.Description;
+import codedriver.framework.restful.annotation.Input;
+import codedriver.framework.restful.annotation.OperationType;
+import codedriver.framework.restful.annotation.Output;
+import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import codedriver.module.cmdb.auth.label.CIENTITY_MODIFY;
@@ -19,20 +33,13 @@ import codedriver.module.cmdb.auth.label.CI_MODIFY;
 import codedriver.module.cmdb.auth.label.CMDB_BASE;
 import codedriver.module.cmdb.service.ci.CiAuthChecker;
 import codedriver.module.cmdb.service.cientity.CiEntityService;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @AuthAction(action = CMDB_BASE.class)
 @AuthAction(action = CI_MODIFY.class)
 @AuthAction(action = CIENTITY_MODIFY.class)
 @OperationType(type = OperationTypeEnum.DELETE)
-public class BatchDeleteCiEntityApi extends PrivateApiComponentBase {
+public class BatchDeleteCiEntityApi extends PrivateApiComponentBase implements IBatchDeleteCiEntityApiCrossoverService {
 
     @Autowired
     private CiEntityService ciEntityService;
