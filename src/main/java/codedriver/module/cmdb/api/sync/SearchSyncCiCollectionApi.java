679d4f2708ba03d07de6d6d807eeace2c7a231c5
陈其炜 <chenqw@techsure.com.cn>
2022-02-15 14:43:09 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/module/cmdb/api/sync/SearchSyncCiCollectionApi.java
+++ src/main/java/codedriver/module/cmdb/api/sync/SearchSyncCiCollectionApi.java
@@ -1,11 +1,12 @@
 /*
- * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
 package codedriver.module.cmdb.api.sync;
 
 import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.cmdb.dto.sync.CollectionVo;
 import codedriver.framework.cmdb.dto.sync.SyncCiCollectionVo;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.restful.annotation.Description;
@@ -18,9 +19,14 @@ import codedriver.framework.util.TableResultUtil;
 import codedriver.module.cmdb.auth.label.CMDB_BASE;
 import codedriver.module.cmdb.service.sync.SyncService;
 import com.alibaba.fastjson.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -28,8 +34,10 @@ import java.util.List;
 @OperationType(type = OperationTypeEnum.SEARCH)
 public class SearchSyncCiCollectionApi extends PrivateApiComponentBase {
 
-    @Autowired
+    @Resource
     private SyncService syncService;
+    @Resource
+    private MongoTemplate mongoTemplate;
 
     @Override
     public String getToken() {
@@ -46,12 +54,22 @@ public class SearchSyncCiCollectionApi extends PrivateApiComponentBase {
         return null;
     }
 
-    @Input({@Param(name = "ciId", type = ApiParamType.LONG, desc = "模型id"),
-            @Param(name = "collectionName", type = ApiParamType.STRING, desc = "集合名称")})
+    @Input({@Param(name = "ciId", type = ApiParamType.LONG, desc = "模型id"), @Param(name = "collectionType", type = ApiParamType.STRING, desc = "集合类型"), @Param(name = "collectionName", type = ApiParamType.STRING, desc = "集合名称")})
     @Description(desc = "搜索模型集合映射接口")
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
         SyncCiCollectionVo syncCiCollectionVo = JSONObject.toJavaObject(jsonObj, SyncCiCollectionVo.class);
+        if (StringUtils.isNotBlank(jsonObj.getString("collectionType")) && StringUtils.isBlank(jsonObj.getString("collectionName"))) {
+            Query query = new Query();
+            Criteria criteria = Criteria.where("collection").is(jsonObj.getString("collectionType"));
+            query.addCriteria(criteria);
+            List<CollectionVo> collectionList = mongoTemplate.find(query, CollectionVo.class, "_dictionary");
+            List<String> collectionNameList = new ArrayList<>();
+            for (CollectionVo collectionVo : collectionList) {
+                collectionNameList.add(collectionVo.getName());
+            }
+            syncCiCollectionVo.setCollectionNameList(collectionNameList);
+        }
         List<SyncCiCollectionVo> syncCiCollectionList = syncService.searchSyncCiCollection(syncCiCollectionVo);
         return TableResultUtil.getResult(syncCiCollectionList, syncCiCollectionVo);
     }
