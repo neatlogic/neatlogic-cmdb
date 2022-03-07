0ab26a560903e26a37cf9d103a4372eb83ea6e3a
陈其炜 <chenqw@techsure.com.cn>
2022-03-07 17:40:33 +0800

    --bug=1009161 --user=陈其炜 批量导入-必填的关系类型为空,配置项导入成功 https://www.tapd.cn/54247054/s/1212492
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/module/cmdb/api/ci/GetImportFieldListApi.java
+++ src/main/java/codedriver/module/cmdb/api/ci/GetImportFieldListApi.java
@@ -1,5 +1,5 @@
 /*
- * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
@@ -61,6 +61,12 @@ public class GetImportFieldListApi extends PrivateApiComponentBase {
         }
         if (CollectionUtils.isNotEmpty(ciVo.getAttrList())) {
             ciVo.getAttrList().removeIf(attrVo -> !attrVo.getCanImport());
+        }
+        //唯一表达式的属性也需要设为必填
+        if (CollectionUtils.isNotEmpty(ciVo.getUniqueAttrIdList())) {
+            for (Long attrId : ciVo.getUniqueAttrIdList()) {
+                ciVo.getAttrById(attrId).setIsRequired(1);
+            }
         }
         return ciVo;
     }
