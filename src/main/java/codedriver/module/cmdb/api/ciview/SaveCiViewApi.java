4f7daacd8b4898ee326e04c2428cfba52ade4963
陈其炜 <chenqw@techsure.com.cn>
2022-04-08 11:11:50 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/module/cmdb/api/ciview/SaveCiViewApi.java
+++ src/main/java/codedriver/module/cmdb/api/ciview/SaveCiViewApi.java
@@ -1,5 +1,5 @@
 /*
- * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
@@ -70,6 +70,7 @@ public class SaveCiViewApi extends PrivateApiComponentBase {
                 ciViewMapper.insertCiView(ciViewVo);
             }
         }
+        //System.out.println(System.currentTimeMillis() + ":update ciview");
         return null;
     }
 
