7ab236d5e755eaac1a9f4784448a706ea801f9bb
longrf <a5739119>
2022-04-14 15:48:40 +0800

    --bug=1009514 --user=龙儒锋 资源中心数据初始化、账号管理、资产清单权限前后端不一致 https://www.tapd.cn/54247054/s/1221049

--- src/main/java/codedriver/module/cmdb/api/resourcecenter/account/AccountProtocolSaveApi.java
+++ src/main/java/codedriver/module/cmdb/api/resourcecenter/account/AccountProtocolSaveApi.java
@@ -8,7 +8,7 @@ import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
-import codedriver.module.cmdb.auth.label.RESOURCECENTER_MODIFY;
+import codedriver.module.cmdb.auth.label.RESOURCECENTER_ACCOUNT_MODIFY;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Service;
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 
 @Service
-@AuthAction(action = RESOURCECENTER_MODIFY.class)
+@AuthAction(action = RESOURCECENTER_ACCOUNT_MODIFY.class)
 @OperationType(type = OperationTypeEnum.UPDATE)
 public class AccountProtocolSaveApi extends PrivateApiComponentBase {
 
