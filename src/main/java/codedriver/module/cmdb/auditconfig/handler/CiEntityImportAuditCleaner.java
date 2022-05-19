d23bf8bf3cbe4e926da76a871e47942397448be9
陈其炜 <chenqw@techsure.com.cn>
2022-05-19 15:54:15 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

---
+++ src/main/java/codedriver/module/cmdb/auditconfig/handler/CiEntityImportAuditCleaner.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.module.cmdb.auditconfig.handler;
+
+import codedriver.framework.auditconfig.core.AuditCleanerBase;
+import codedriver.module.cmdb.dao.mapper.batchimport.ImportMapper;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component
+public class CiEntityImportAuditCleaner extends AuditCleanerBase {
+    @Resource
+    private ImportMapper importMapper;
+
+    @Override
+    public String getName() {
+        return "CIENTITY-IMPORT-AUDIT";
+    }
+
+    @Override
+    protected void myClean(int dayBefore) {
+        importMapper.deleteAuditByDayBefore(dayBefore);
+    }
+}
