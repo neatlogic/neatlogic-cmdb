5bc71044553525bc482e5e4dd05a5f16a40c7d84
陈其炜 <chenqw@techsure.com.cn>
2022-05-31 17:25:29 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/module/cmdb/api/customview/SearchCustomViewDataApi.java
+++ src/main/java/codedriver/module/cmdb/api/customview/SearchCustomViewDataApi.java
@@ -1,5 +1,5 @@
 /*
- * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
@@ -44,10 +44,15 @@ public class SearchCustomViewDataApi extends PrivateApiComponentBase {
 
     @Input({@Param(name = "id", type = ApiParamType.LONG, desc = "视图id", isRequired = true),
             @Param(name = "searchMode", type = ApiParamType.ENUM, rule = "normal,group", isRequired = true, desc = "搜索模式：normal或group"),
-            @Param(name = "groupBy", type = ApiParamType.STRING, desc = "分组属性的uuid")
+            @Param(name = "groupBy", type = ApiParamType.STRING, desc = "分组属性的uuid"),
+            @Param(name = "attrFilterList", type = ApiParamType.JSONARRAY, desc = "属性过滤条件"),
+            @Param(name = "keyword", type = ApiParamType.STRING, desc = "关键字"),
+            @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "当前页"),
+            @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "每页大小")
     })
     @Output({@Param(name = "dataList", type = ApiParamType.JSONARRAY, desc = "结果集"),
             @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "每页大小")})
+    @Example(example = "{\"id\":588094621147136,\"keyword\":\"\",\"pageSize\":20,\"searchMode\":\"normal\",\"currentPage\":1,\"attrFilterList\":[{\"attrUuid\":\"546d7fb7276e40f889cd131e22bb547a\",\"valueList\":[\"192.168.0.22\"],\"expression\":\"like\",\"type\":\"attr\"}]}")
     @Description(desc = "查询自定义视图数据接口")
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
