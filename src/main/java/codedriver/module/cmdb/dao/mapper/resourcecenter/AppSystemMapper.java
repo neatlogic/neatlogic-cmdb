99e0e173f5e2524c034d1b001070ce9beb0fbd86
lvzk <lvzk@techsure.com.cn>
2022-07-18 19:57:04 +0800

    update deploy job

--- src/main/java/codedriver/module/cmdb/dao/mapper/resourcecenter/AppSystemMapper.java
+++ src/main/java/codedriver/module/cmdb/dao/mapper/resourcecenter/AppSystemMapper.java
@@ -8,12 +8,19 @@ package codedriver.module.cmdb.dao.mapper.resourcecenter;
 import codedriver.framework.cmdb.crossover.IAppSystemMapper;
 import codedriver.framework.cmdb.dto.resourcecenter.ResourceSearchVo;
 import codedriver.framework.cmdb.dto.resourcecenter.entity.AppEnvironmentVo;
+import codedriver.framework.cmdb.dto.resourcecenter.entity.AppModuleVo;
+import codedriver.framework.cmdb.dto.resourcecenter.entity.AppSystemVo;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface AppSystemMapper extends IAppSystemMapper {
 //    List<AppSystemVo> searchAppSystem(AppSystemVo appSystemVo);
+
+    AppSystemVo getAppSystemByAbbrName(@Param("abbrName")String abbrName, @Param("schemaName") String schemaName);
+
+    AppModuleVo getAppModuleByAbbrName(@Param("abbrName")String abbrName, @Param("schemaName") String schemaName);
+
     List<AppEnvironmentVo> getAppEnvListByAppSystemIdAndModuleIdList(@Param("appResourceId") Long appResourceId, @Param("moduleResourceIdList") List<Long> moduleIdList, @Param("schemaName") String schemaName);
 
     Integer getAppSystemIdListCount(ResourceSearchVo searchVo);
