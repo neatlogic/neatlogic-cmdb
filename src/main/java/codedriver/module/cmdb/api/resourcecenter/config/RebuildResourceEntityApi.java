9ba56738a2b6b151838e71941e110d08d1de8b83
陈其炜 <chenqw@techsure.com.cn>
2022-07-20 17:57:31 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/module/cmdb/api/resourcecenter/config/RebuildResourceEntityApi.java
+++ src/main/java/codedriver/module/cmdb/api/resourcecenter/config/RebuildResourceEntityApi.java
@@ -8,12 +8,8 @@ package codedriver.module.cmdb.api.resourcecenter.config;
 import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.cmdb.dto.resourcecenter.config.ResourceEntityVo;
 import codedriver.framework.cmdb.enums.resourcecenter.Status;
-import codedriver.framework.cmdb.exception.resourcecenter.ResourceCenterResourceFoundException;
-import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.restful.annotation.Description;
-import codedriver.framework.restful.annotation.Input;
 import codedriver.framework.restful.annotation.OperationType;
-import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import codedriver.module.cmdb.auth.label.RESOURCECENTER_MODIFY;
@@ -25,7 +21,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * @author linbq
@@ -35,18 +30,18 @@ import java.util.Objects;
 @AuthAction(action = RESOURCECENTER_MODIFY.class)
 @OperationType(type = OperationTypeEnum.OPERATE)
 @Transactional
-public class InitResourceEntityApi extends PrivateApiComponentBase {
+public class RebuildResourceEntityApi extends PrivateApiComponentBase {
     @Resource
     private ResourceEntityMapper resourceEntityMapper;
 
     @Override
     public String getToken() {
-        return "resourcecenter/resourceentity/init";
+        return "resourcecenter/resourceentity/rebuild";
     }
 
     @Override
     public String getName() {
-        return "初始化资源配置信息";
+        return "重建所有资源视图";
     }
 
     @Override
@@ -54,9 +49,7 @@ public class InitResourceEntityApi extends PrivateApiComponentBase {
         return null;
     }
 
-    @Input({
-    })
-    @Description(desc = "初始化资源配置信息")
+    @Description(desc = "重建所有资源视图接口")
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
         List<ResourceEntityVo> resourceEntityList = resourceEntityMapper.getAllResourceEntity();
