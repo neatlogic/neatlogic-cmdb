5b40e21fb88ad080fc1721d1ea35bc2afa7cda1d
linbq@techsure.com.cn <Besiter789>
2022-08-05 10:28:49 +0800

    --bug=1009972 --user=林邦泉 标签管理-添加和编辑标签失败 https://www.tapd.cn/54247054/s/1245027

--- src/main/java/codedriver/module/cmdb/api/resourcecenter/tag/TagSaveApi.java
+++ src/main/java/codedriver/module/cmdb/api/resourcecenter/tag/TagSaveApi.java
@@ -6,6 +6,9 @@
 package codedriver.module.cmdb.api.resourcecenter.tag;
 
 import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.cmdb.dto.tag.TagVo;
+import codedriver.framework.cmdb.exception.resourcecenter.ResourceCenterTagNameRepeatsException;
+import codedriver.framework.cmdb.exception.resourcecenter.ResourceCenterTagNotFoundException;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.dto.FieldValidResultVo;
 import codedriver.framework.restful.annotation.*;
@@ -13,13 +16,19 @@ import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.IValid;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import codedriver.module.cmdb.auth.label.RESOURCECENTER_TAG_MODIFY;
+import codedriver.module.cmdb.dao.mapper.resourcecenter.ResourceTagMapper;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 @AuthAction(action = RESOURCECENTER_TAG_MODIFY.class)
 @OperationType(type = OperationTypeEnum.OPERATE)
 public class TagSaveApi extends PrivateApiComponentBase {
+
+    @Resource
+    private ResourceTagMapper resourceTagMapper;
 
     @Override
     public String getToken() {
@@ -47,28 +56,28 @@ public class TagSaveApi extends PrivateApiComponentBase {
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
         JSONObject resultObj = new JSONObject();
-//        TagVo tagVo = JSON.toJavaObject(paramObj, TagVo.class);
-//        Long id = paramObj.getLong("id");
-//        if (resourceCenterMapper.checkTagNameIsRepeats(tagVo) > 0) {
-//            throw new ResourceCenterTagNameRepeatsException(tagVo.getName());
-//        }
-//        if (id != null) {
-//            if (resourceCenterMapper.checkTagIsExistsById(id) == 0) {
-//                throw new ResourceCenterTagNotFoundException(id);
-//            }
-//            resourceCenterMapper.updateTag(tagVo);
-//        } else {
-//            resourceCenterMapper.insertTag(tagVo);
-//        }
+        TagVo tagVo = JSONObject.toJavaObject(paramObj, TagVo.class);
+        Long id = paramObj.getLong("id");
+        if (resourceTagMapper.checkTagNameIsRepeats(tagVo) > 0) {
+            throw new ResourceCenterTagNameRepeatsException(tagVo.getName());
+        }
+        if (id != null) {
+            if (resourceTagMapper.checkTagIsExistsById(id) == 0) {
+                throw new ResourceCenterTagNotFoundException(id);
+            }
+            resourceTagMapper.updateTag(tagVo);
+        } else {
+            resourceTagMapper.insertTag(tagVo);
+        }
         return resultObj;
     }
 
     public IValid name() {
         return value -> {
-//            TagVo vo = JSON.toJavaObject(value, TagVo.class);
-//            if (resourceCenterMapper.checkTagNameIsRepeats(vo) > 0) {
-//                return new FieldValidResultVo(new ResourceCenterTagNameRepeatsException(vo.getName()));
-//            }
+            TagVo vo = JSONObject.toJavaObject(value, TagVo.class);
+            if (resourceTagMapper.checkTagNameIsRepeats(vo) > 0) {
+                return new FieldValidResultVo(new ResourceCenterTagNameRepeatsException(vo.getName()));
+            }
             return new FieldValidResultVo();
         };
     }
