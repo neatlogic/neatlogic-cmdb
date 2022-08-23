767f8c38d0435cc1d818acd49696d6407e6962d5
赖文韬 <laiwt@techsure.com.cn>
2022-08-23 15:03:29 +0800

    --story=1006564 --user=赖文韬 配置管理-资产清单-账户管理  在账户管理弹窗中增加【连接测试】功能 https://www.tapd.cn/54247054/s/1248492

--- src/main/java/codedriver/module/cmdb/api/resourcecenter/resource/ResourceAccountAccessTestApi.java
+++ src/main/java/codedriver/module/cmdb/api/resourcecenter/resource/ResourceAccountAccessTestApi.java
@@ -7,18 +7,30 @@ package codedriver.module.cmdb.api.resourcecenter.resource;
 
 import codedriver.framework.asynchronization.threadlocal.TenantContext;
 import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.autoexec.exception.AutoexecJobRunnerGroupRunnerNotFoundException;
+import codedriver.framework.autoexec.exception.AutoexecJobRunnerNotMatchException;
 import codedriver.framework.cmdb.dto.resourcecenter.AccountVo;
 import codedriver.framework.cmdb.dto.resourcecenter.ResourceVo;
+import codedriver.framework.cmdb.exception.resourcecenter.ResourceAccountAccessTestException;
 import codedriver.framework.cmdb.exception.resourcecenter.ResourceNotFoundException;
 import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.common.util.IpUtil;
+import codedriver.framework.dao.mapper.runner.RunnerMapper;
+import codedriver.framework.dto.runner.GroupNetworkVo;
+import codedriver.framework.dto.runner.RunnerGroupVo;
+import codedriver.framework.dto.runner.RunnerMapVo;
+import codedriver.framework.integration.authentication.enums.AuthenticateType;
 import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import codedriver.framework.util.HttpRequestUtil;
 import codedriver.module.cmdb.auth.label.CMDB_BASE;
 import codedriver.module.cmdb.dao.mapper.resourcecenter.ResourceAccountMapper;
 import codedriver.module.cmdb.dao.mapper.resourcecenter.ResourceMapper;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -34,6 +46,9 @@ public class ResourceAccountAccessTestApi extends PrivateApiComponentBase {
 
     @Resource
     private ResourceAccountMapper resourceAccountMapper;
+
+    @Resource
+    RunnerMapper runnerMapper;
 
     @Override
     public String getToken() {
@@ -64,6 +79,24 @@ public class ResourceAccountAccessTestApi extends PrivateApiComponentBase {
         if (resource == null) {
             throw new ResourceNotFoundException(resourceId);
         }
+        // 根据网段查找runner
+        List<RunnerMapVo> runnerMapList = null;
+        List<GroupNetworkVo> networkVoList = runnerMapper.getAllNetworkMask();
+        for (GroupNetworkVo networkVo : networkVoList) {
+            if (IpUtil.isBelongSegment(resource.getIp(), networkVo.getNetworkIp(), networkVo.getMask())) {
+                RunnerGroupVo groupVo = runnerMapper.getRunnerMapGroupById(networkVo.getGroupId());
+                if (CollectionUtils.isEmpty(groupVo.getRunnerMapList())) {
+                    throw new AutoexecJobRunnerGroupRunnerNotFoundException(groupVo.getName() + "(" + networkVo.getGroupId() + ") ");
+                }
+                runnerMapList = groupVo.getRunnerMapList();
+            }
+        }
+        if (CollectionUtils.isEmpty(runnerMapList)) {
+            throw new AutoexecJobRunnerNotMatchException();
+        }
+        // 随机分配runner
+        int runnerMapIndex = (int) (Math.random() * runnerMapList.size());
+        RunnerMapVo runnerMapVo = runnerMapList.get(runnerMapIndex);
         JSONArray list = new JSONArray();
         List<AccountVo> accountList = resourceAccountMapper.getResourceAccountListByResourceId(resourceId, null);
         if (accountList.size() > 0) {
@@ -71,18 +104,28 @@ public class ResourceAccountAccessTestApi extends PrivateApiComponentBase {
                 list.add(new JSONObject() {
                     {
                         this.put("resourceId", resourceId);
-                        this.put("host", resource.getIp());
-                        this.put("protocolPort", vo.getProtocolPort());
-                        this.put("protocol", vo.getProtocol());
-                        this.put("nodeName", resource.getName());
-                        this.put("nodeType", resource.getTypeName());
-                        this.put("username", vo.getAccount());
-                        this.put("password", vo.getPasswordCipher());
+                        this.put("host", resource.getIp() != null ? resource.getIp() : "");
+                        this.put("port", vo.getProtocolPort() != null ? vo.getProtocolPort() : "");
+                        this.put("protocolPort", vo.getProtocolPort() != null ? vo.getProtocolPort() : "");
+                        this.put("protocol", vo.getProtocol() != null ? vo.getProtocol() : "");
+                        this.put("nodeName", resource.getName() != null ? resource.getName() : "");
+                        this.put("nodeType", resource.getTypeName() != null ? resource.getTypeName() : "");
+                        this.put("username", vo.getAccount() != null ? vo.getAccount() : null);
+                        this.put("password", vo.getPasswordCipher() != null ? vo.getPasswordCipher() : "");
                     }
                 });
             }
         }
-        return null;
+        String url = runnerMapVo.getUrl() + "api/rest/account/accesstest";
+        JSONObject paramJson = new JSONObject();
+        paramJson.put("accountList", list);
+        HttpRequestUtil request = HttpRequestUtil.post(url).setPayload(paramJson.toJSONString()).setAuthType(AuthenticateType.BUILDIN).sendRequest();
+        JSONObject resultJson = request.getResultJson();
+        String error = request.getError();
+        if (StringUtils.isNotBlank(error)) {
+            throw new ResourceAccountAccessTestException(error);
+        }
+        return resultJson.getJSONArray("Return");
     }
 
 }
