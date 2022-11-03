a1b320d18ef75030d995110d35f4ae2c850fc31d
陈其炜 <chenqw@techsure.com.cn>
2022-11-03 11:37:16 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/module/cmdb/api/ci/DeleteCiApi.java
+++ src/main/java/codedriver/module/cmdb/api/ci/DeleteCiApi.java
@@ -1,26 +1,26 @@
 /*
- * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
 package codedriver.module.cmdb.api.ci;
 
 import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.cmdb.auth.label.CI_MODIFY;
+import codedriver.framework.cmdb.auth.label.CMDB_BASE;
 import codedriver.framework.cmdb.dto.ci.CiVo;
 import codedriver.framework.cmdb.exception.ci.CiAuthException;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
-import codedriver.framework.cmdb.auth.label.CI_MODIFY;
-import codedriver.framework.cmdb.auth.label.CMDB_BASE;
-import codedriver.module.cmdb.dao.mapper.ci.CiMapper;
 import codedriver.module.cmdb.service.ci.CiAuthChecker;
 import codedriver.module.cmdb.service.ci.CiService;
 import com.alibaba.fastjson.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
 
 @Service
 @AuthAction(action = CMDB_BASE.class)
@@ -29,10 +29,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class DeleteCiApi extends PrivateApiComponentBase {
 
-    @Autowired
-    private CiMapper ciMapper;
 
-    @Autowired
+    @Resource
     private CiService ciService;
 
 
