bc25e28368721bc9b3ef1b125b70ffb7d46fa25b
linbq@techsure.com.cn <Besiter789>
2022-12-28 18:29:05 +0800

    --story=1007198 --user=林邦泉 流程管理-导入流程时通知策略匹配不成功，提示通知策略所在路径及名称 https://www.tapd.cn/54247054/s/1264807

--- src/main/java/codedriver/module/cmdb/process/stephandler/CIEntitySyncProcessUtilHandler.java
+++ src/main/java/codedriver/module/cmdb/process/stephandler/CIEntitySyncProcessUtilHandler.java
@@ -46,10 +46,7 @@ public class CIEntitySyncProcessUtilHandler extends ProcessStepInternalHandlerBa
         JSONObject notifyPolicyConfig = stepConfigObj.getJSONObject("notifyPolicyConfig");
         NotifyPolicyConfigVo notifyPolicyConfigVo = JSONObject.toJavaObject(notifyPolicyConfig, NotifyPolicyConfigVo.class);
         if (notifyPolicyConfigVo != null) {
-            Long policyId = notifyPolicyConfigVo.getPolicyId();
-            if (policyId != null) {
-                processStepVo.setNotifyPolicyId(policyId);
-            }
+            processStepVo.setNotifyPolicyConfig(notifyPolicyConfigVo);
         }
 
         JSONObject actionConfig = stepConfigObj.getJSONObject("actionConfig");
