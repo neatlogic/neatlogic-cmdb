de2b1efd6372667d05c5f3ef8ba879b9959b986f
陈其炜 <chenqw@techsure.com.cn>
2023-01-03 15:50:19 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/module/cmdb/api/ci/ExportCiForExcelApi.java
+++ src/main/java/codedriver/module/cmdb/api/ci/ExportCiForExcelApi.java
@@ -1,5 +1,5 @@
 /*
- * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
+ * Copyright(c) 2023 TechSure Co., Ltd. All Rights Reserved.
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
@@ -102,6 +102,8 @@ public class ExportCiForExcelApi extends PrivateBinaryStreamApiComponentBase {
             workbook.write(os);
         } catch (IOException e) {
             logger.error(e.getMessage(), e);
+        } finally {
+            workbook.close();
         }
         return null;
     }
