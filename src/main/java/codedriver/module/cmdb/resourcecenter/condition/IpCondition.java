4d070db31ba37b658e9bceb9568ee428d5b916b5
吕佐康 <lvzk@techsure.com.cn>
2023-01-11 11:00:00 +0800

    --story=1007143 --user=lvzk 自动化 -- 执行目标选择（包括所有入口）-- 过滤器方式 --增加【高级模式】选项 https://www.tapd.cn/54247054/s/1264750

---
+++ src/main/java/codedriver/module/cmdb/resourcecenter/condition/IpCondition.java
@@ -0,0 +1,33 @@
+package codedriver.module.cmdb.resourcecenter.condition;
+
+import codedriver.framework.cmdb.resourcecenter.condition.ResourcecenterConditionBase;
+import codedriver.framework.cmdb.resourcecenter.table.ScenceIpobjectDetailTable;
+import codedriver.framework.dto.condition.ConditionVo;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class IpCondition extends ResourcecenterConditionBase {
+
+    @Override
+    public String getName() {
+        return "ip";
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "ip";
+    }
+
+    @Override
+    public Object valueConversionText(Object value, JSONObject config) {
+        return value;
+    }
+
+    @Override
+    public void getSqlConditionWhere(List<ConditionVo> conditionList, Integer index, StringBuilder sqlSb) {
+        getSimpleSqlConditionWhere(conditionList.get(index), sqlSb, new ScenceIpobjectDetailTable().getShortName(), ScenceIpobjectDetailTable.FieldEnum.IP.getValue());
+    }
+}
