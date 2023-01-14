527cad3d2a8d07a9d4d9d608f50b9bae243eacce
陈其炜 <chenqw@techsure.com.cn>
2023-01-14 17:32:22 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/module/cmdb/utils/CustomViewBuilder.java
+++ src/main/java/codedriver/module/cmdb/utils/CustomViewBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright(c) 2022 TechSure Co., Ltd. All Rights Reserved.
+ * Copyright(c) 2023 TechSure Co., Ltd. All Rights Reserved.
  * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
  */
 
@@ -246,6 +246,20 @@ public class CustomViewBuilder {
             plainSelect.addSelectItems(new SelectExpressionItem(new Column("id").withTable(new Table("ci_base"))));
             plainSelect.addSelectItems(new SelectExpressionItem(new Column("name").withTable(new Table("ci_base"))));
             plainSelect.addSelectItems(new SelectExpressionItem(new StringValue(ciVo.getName())).withAlias(new Alias("ciName")));
+            Function lcdFuc = new Function();
+            lcdFuc.setName("DATE_FORMAT");
+            ExpressionList lcdExpressionList = new ExpressionList();
+            lcdExpressionList.addExpressions(new Column("lcd").withTable(new Table("ci_base")));
+            lcdExpressionList.addExpressions(new StringValue("%Y-%m-%d"));
+            lcdFuc.setParameters(lcdExpressionList);
+            plainSelect.addSelectItems(new SelectExpressionItem(lcdFuc).withAlias(new Alias("lcd")));
+            Function fcdFuc = new Function();
+            fcdFuc.setName("DATE_FORMAT");
+            ExpressionList fcdExpressionList = new ExpressionList();
+            fcdExpressionList.addExpressions(new Column("fcd").withTable(new Table("ci_base")));
+            fcdExpressionList.addExpressions(new StringValue("%Y-%m-%d"));
+            fcdFuc.setParameters(fcdExpressionList);
+            plainSelect.addSelectItems(new SelectExpressionItem(fcdFuc).withAlias(new Alias("fcd")));
             //plainSelect.addSelectItems(new SelectExpressionItem(new Column("name").withTable(new Table("cmdb_ci"))).withAlias(new Alias("ciName")));
             plainSelect.addSelectItems(new SelectExpressionItem(new Column("uuid").withTable(new Table("ci_base"))));
 
