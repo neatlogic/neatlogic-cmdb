a5d467f1c1f7d584f44100ba0c4bba9d290689ee
linbangquan <1437892690@qq.com>
2023-08-15 15:17:13 +0800

    [功能] 场景视图字段固化到代码中
    关联 #[957880973361152]场景视图字段固化到代码中 http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/957880973361152

--- src/main/java/neatlogic/module/cmdb/api/resourcecenter/config/ListResourceEntityApi.java
+++ src/main/java/neatlogic/module/cmdb/api/resourcecenter/config/ListResourceEntityApi.java
@@ -18,6 +18,9 @@ package neatlogic.module.cmdb.api.resourcecenter.config;
 
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.cmdb.dto.resourcecenter.config.ResourceEntityVo;
+import neatlogic.framework.cmdb.dto.resourcecenter.config.SceneEntityVo;
+import neatlogic.framework.cmdb.enums.resourcecenter.Status;
+import neatlogic.framework.cmdb.enums.resourcecenter.ViewType;
 import neatlogic.framework.restful.annotation.Description;
 import neatlogic.framework.restful.annotation.OperationType;
 import neatlogic.framework.restful.annotation.Output;
@@ -27,9 +30,14 @@ import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
 import neatlogic.framework.cmdb.auth.label.RESOURCECENTER_MODIFY;
 import neatlogic.module.cmdb.dao.mapper.resourcecenter.ResourceEntityMapper;
 import com.alibaba.fastjson.JSONObject;
+import neatlogic.module.cmdb.utils.ResourceEntityFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author linbq
@@ -50,7 +58,7 @@ public class ListResourceEntityApi extends PrivateApiComponentBase {
 
     @Override
     public String getName() {
-        return "获取资源中心配置列表";
+        return "nmcarc.listresourceentityapi.getname";
     }
 
     @Override
@@ -58,10 +66,28 @@ public class ListResourceEntityApi extends PrivateApiComponentBase {
         return null;
     }
 
-    @Output({@Param(name = "Return", explode = ResourceEntityVo[].class)})
-    @Description(desc = "获取资源中心配置列表接口")
+    @Output({
+            @Param(name = "Return", explode = ResourceEntityVo[].class, desc = "common.tbodylist")
+    })
+    @Description(desc = "nmcarc.listresourceentityapi.getname")
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
-        return resourceEntityMapper.getAllResourceEntity();
+        List<ResourceEntityVo> resultList = new ArrayList<>();
+        List<String> viewNameList = ResourceEntityFactory.getViewNameList();
+        List<ResourceEntityVo> resourceEntityList = resourceEntityMapper.getResourceEntityListByNameList(viewNameList);
+        Map<String, ResourceEntityVo> resourceEntityVoMap = resourceEntityList.stream().collect(Collectors.toMap(e -> e.getName(), e -> e));
+        List<SceneEntityVo> sceneEntityList = ResourceEntityFactory.getSceneEntityList();
+        for (SceneEntityVo sceneEntityVo : sceneEntityList) {
+            ResourceEntityVo resourceEntityVo = resourceEntityVoMap.get(sceneEntityVo.getName());
+            if (resourceEntityVo == null) {
+                resourceEntityVo = new ResourceEntityVo();
+                resourceEntityVo.setName(sceneEntityVo.getName());
+                resourceEntityVo.setLabel(sceneEntityVo.getLabel());
+                resourceEntityVo.setStatus(Status.PENDING.getValue());
+                resourceEntityVo.setType(ViewType.SCENE.getValue());
+            }
+            resultList.add(resourceEntityVo);
+        }
+        return resultList;
     }
 }
