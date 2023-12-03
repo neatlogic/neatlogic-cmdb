6fc622107bc528bc1aeb2a9a64ce754a33256df9
chenqw <zenkiray@live.com>
2023-12-03 19:37:57 +0800

    adjust
    Signed-off-by: chenqw <zenkiray@live.com>

---
+++ src/main/java/neatlogic/module/cmdb/api/cientity/GetCiEntityBaseInfoApi.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package neatlogic.module.cmdb.api.cientity;
+
+import com.alibaba.fastjson.JSONObject;
+import neatlogic.framework.auth.core.AuthAction;
+import neatlogic.framework.cmdb.auth.label.CMDB_BASE;
+import neatlogic.framework.cmdb.dto.cientity.CiEntityVo;
+import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.restful.annotation.*;
+import neatlogic.framework.restful.constvalue.OperationTypeEnum;
+import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
+import neatlogic.module.cmdb.dao.mapper.cientity.CiEntityMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+@Service
+@AuthAction(action = CMDB_BASE.class)
+@OperationType(type = OperationTypeEnum.SEARCH)
+@Transactional
+public class GetCiEntityBaseInfoApi extends PrivateApiComponentBase {
+
+    @Resource
+    private CiEntityMapper ciEntityMapper;
+
+
+    @Override
+    public String getToken() {
+        return "/cmdb/cientity/baseinfo/get";
+    }
+
+    @Override
+    public String getName() {
+        return "nmcac.getcientitybaseinfoapi.getname";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({@Param(name = "id", type = ApiParamType.LONG, isRequired = true, desc = "term.cmdb.cientityid")})
+    @Output({@Param(name = "Return", explode = CiEntityVo.class)})
+    @Description(desc = "nmcac.getcientitybaseinfoapi.getname")
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+        Long id = jsonObj.getLong("id");
+        return ciEntityMapper.getCiEntityBaseInfoById(id);
+    }
+
+}
