736a52ea93f206dff7d8a3c86d2de61e69faf3cb
chenqw <zenkiray@live.com>
2023-12-13 16:22:19 +0800

    adjust
    Signed-off-by: chenqw <zenkiray@live.com>

--- src/main/resources/neatlogic/resources/cmdb/changelog/2023-12-23/neatlogic_tenant.sql
+++ src/main/resources/neatlogic/resources/cmdb/changelog/2023-12-23/neatlogic_tenant.sql
@@ -1,3 +1,2 @@
-
-ALTER TABLE `neatlogic_develop`.`cmdb_group`
+ALTER TABLE `cmdb_group`
     MODIFY COLUMN `type` enum ('readonly','maintain','autoexec') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '类型' AFTER `name`;
