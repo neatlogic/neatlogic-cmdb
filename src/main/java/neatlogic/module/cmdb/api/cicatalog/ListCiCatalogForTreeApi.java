60d1e2151686cbba5beb91bdcc30955cbdb6de8c
linbangquan <1437892690@qq.com>
2023-12-19 11:54:14 +0800

    [功能] 后端-CMDB-模型详情页面、配置项查询的详情页面，增加树目录
    关联 #[1047056406249472]后端-CMDB-模型详情页面、配置项查询的详情页面，增加树目录 http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/1047056406249472

--- src/main/java/neatlogic/module/cmdb/api/cicatalog/ListCiCatalogForTreeApi.java
+++ src/main/java/neatlogic/module/cmdb/api/cicatalog/ListCiCatalogForTreeApi.java
@@ -3,16 +3,20 @@ package neatlogic.module.cmdb.api.cicatalog;
 import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.cmdb.auth.label.CMDB_BASE;
+import neatlogic.framework.cmdb.dto.ci.CiVo;
 import neatlogic.framework.cmdb.dto.cicatalog.CiCatalogNodeVo;
 import neatlogic.framework.cmdb.dto.cicatalog.CiCatalogVo;
 import neatlogic.framework.restful.annotation.*;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
+import neatlogic.module.cmdb.dao.mapper.ci.CiMapper;
 import neatlogic.module.cmdb.service.cicatalog.CiCatalogService;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -21,6 +25,9 @@ import java.util.stream.Collectors;
 @AuthAction(action = CMDB_BASE.class)
 @OperationType(type = OperationTypeEnum.SEARCH)
 public class ListCiCatalogForTreeApi extends PrivateApiComponentBase {
+
+    @Resource
+    private CiMapper ciMapper;
 
     @Resource
     private CiCatalogService ciCatalogService;
@@ -41,10 +48,21 @@ public class ListCiCatalogForTreeApi extends PrivateApiComponentBase {
         if (CollectionUtils.isEmpty(allNodeList)) {
             return allNodeList;
         }
+        Map<Long, List<CiVo>> catalogId2CiListMap = new HashMap<>();
+        List<CiVo> ciList = ciMapper.getAllCi(null);
+        for (CiVo ciVo : ciList) {
+            if (ciVo.getCatalogId() != null) {
+                catalogId2CiListMap.computeIfAbsent(ciVo.getCatalogId(), key -> new ArrayList<>()).add(ciVo);
+            }
+        }
         CiCatalogNodeVo rootNode = allNodeList.get(0);
         Map<Long, CiCatalogNodeVo> id2NodeMap = allNodeList.stream().collect(Collectors.toMap(e -> e.getId(), e -> e));
         for (CiCatalogNodeVo node : allNodeList) {
             node.setType(CiCatalogNodeVo.CATALOG);
+            List<CiVo> ciVoList = catalogId2CiListMap.get(node.getId());
+            if (CollectionUtils.isNotEmpty(ciVoList)) {
+                node.setChildrenCount(ciVoList.size());
+            }
             CiCatalogNodeVo parent = id2NodeMap.get(node.getParentId());
             if (parent != null) {
                 parent.addChild(node);
