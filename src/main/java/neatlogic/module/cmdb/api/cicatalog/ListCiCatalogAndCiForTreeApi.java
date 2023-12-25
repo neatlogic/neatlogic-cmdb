2d524c13cc0e0cc9c7e1917d107e0e9999dbc7a1
linbangquan <1437892690@qq.com>
2023-12-25 17:02:17 +0800

    [功能] 模型树目录中的模型增加权限判断逻辑
    关联 #[1054336795246592]模型树目录中的模型增加权限判断逻辑 http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/1054336795246592

--- src/main/java/neatlogic/module/cmdb/api/cicatalog/ListCiCatalogAndCiForTreeApi.java
+++ src/main/java/neatlogic/module/cmdb/api/cicatalog/ListCiCatalogAndCiForTreeApi.java
@@ -2,25 +2,31 @@ package neatlogic.module.cmdb.api.cicatalog;
 
 import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.auth.core.AuthAction;
+import neatlogic.framework.auth.core.AuthActionChecker;
+import neatlogic.framework.cmdb.auth.label.CIENTITY_MODIFY;
+import neatlogic.framework.cmdb.auth.label.CI_MODIFY;
 import neatlogic.framework.cmdb.auth.label.CMDB_BASE;
+import neatlogic.framework.cmdb.dto.ci.CiAuthVo;
 import neatlogic.framework.cmdb.dto.ci.CiVo;
 import neatlogic.framework.cmdb.dto.cicatalog.CiCatalogNodeVo;
 import neatlogic.framework.cmdb.dto.cicatalog.CiCatalogVo;
+import neatlogic.framework.cmdb.enums.CiAuthType;
+import neatlogic.framework.cmdb.enums.group.GroupType;
 import neatlogic.framework.common.constvalue.ApiParamType;
 import neatlogic.framework.restful.annotation.*;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
+import neatlogic.module.cmdb.dao.mapper.ci.CiAuthMapper;
 import neatlogic.module.cmdb.dao.mapper.ci.CiMapper;
+import neatlogic.module.cmdb.service.ci.CiAuthChecker;
 import neatlogic.module.cmdb.service.cicatalog.CiCatalogService;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @AuthAction(action = CMDB_BASE.class)
@@ -29,6 +35,9 @@ public class ListCiCatalogAndCiForTreeApi extends PrivateApiComponentBase {
 
     @Resource
     private CiMapper ciMapper;
+
+    @Resource
+    private CiAuthMapper ciAuthMapper;
 
     @Resource
     private CiCatalogService ciCatalogService;
@@ -47,88 +56,73 @@ public class ListCiCatalogAndCiForTreeApi extends PrivateApiComponentBase {
     @Description(desc = "nmcac.listcicatalogandcifortreeapi.getname")
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
-        CiCatalogNodeVo rootNode = null;
         List<CiCatalogNodeVo> catalogList = new ArrayList<>();
-        Map<Long, CiCatalogNodeVo> id2NodeMap = new HashMap<>();
+        List<CiCatalogNodeVo> matchCiCatalogNodeList = new ArrayList<>();
         List<CiCatalogNodeVo> noCatalogCiNodeList = new ArrayList<>();
-        List<CiCatalogNodeVo> ciNodeList = new ArrayList<>();
+        List<CiCatalogNodeVo> hasCatalogCiNodeList = new ArrayList<>();
         List<CiCatalogNodeVo> allNodeList = ciCatalogService.getAllCiCatalogList();
+        Map<Long, CiCatalogNodeVo> id2NodeMap = allNodeList.stream().collect(Collectors.toMap(e -> e.getId(), e -> e));
         List<CiVo> ciList = ciMapper.getAllCi(null);
         String keyword = paramObj.getString("keyword");
         if (StringUtils.isNotBlank(keyword)) {
             keyword = keyword.toLowerCase();
-            List<CiCatalogNodeVo> matchKeywordCiCatalogNodeList = new ArrayList<>();
-            if (CollectionUtils.isNotEmpty(allNodeList)) {
-                rootNode = allNodeList.get(0);
-                for (CiCatalogNodeVo node : allNodeList) {
-                    node.setType(CiCatalogNodeVo.CATALOG);
-                    id2NodeMap.put(node.getId(), node);
-                    if (node.getName().toLowerCase().contains(keyword)) {
-                        matchKeywordCiCatalogNodeList.add(node);
-                    }
-                }
-            }
-            for (CiVo ciVo : ciList) {
-                if (!ciVo.getName().toLowerCase().contains(keyword) && !ciVo.getLabel().toLowerCase().contains(keyword)) {
-                    continue;
-                }
-                CiCatalogNodeVo ciNode = new CiCatalogNodeVo(ciVo);
-                if (ciVo.getCatalogId() == null) {
-                    noCatalogCiNodeList.add(ciNode);
-                    continue;
-                }
-                CiCatalogNodeVo node = id2NodeMap.get(ciVo.getCatalogId());
-                if (node == null) {
-                    noCatalogCiNodeList.add(ciNode);
-                    continue;
-                }
-                matchKeywordCiCatalogNodeList.add(node);
-                ciNodeList.add(ciNode);
-            }
+            List<Long> matchKeywordCiCatalogNodeIdList = new ArrayList<>();
             for (CiCatalogNodeVo node : allNodeList) {
-                for (CiCatalogNodeVo matchKeywordCiCatalogNode : matchKeywordCiCatalogNodeList) {
-                    if (node.getLft() <= matchKeywordCiCatalogNode.getLft() && node.getRht() >= matchKeywordCiCatalogNode.getRht()) {
-                        catalogList.add(node);
-                    }
+                if (node.getName().toLowerCase().contains(keyword)) {
+                    matchCiCatalogNodeList.add(node);
+                    matchKeywordCiCatalogNodeIdList.add(node.getId());
                 }
             }
-        } else {
-            if (CollectionUtils.isNotEmpty(allNodeList)) {
-                rootNode = allNodeList.get(0);
-                for (CiCatalogNodeVo node : allNodeList) {
-                    node.setType(CiCatalogNodeVo.CATALOG);
-                    id2NodeMap.put(node.getId(), node);
+            Iterator<CiVo> iterator = ciList.iterator();
+            while (iterator.hasNext()) {
+                CiVo ciVo = iterator.next();
+                if (matchKeywordCiCatalogNodeIdList.contains(ciVo.getCatalogId())) {
+                    continue;
+                }
+                if (!ciVo.getName().toLowerCase().contains(keyword) && !ciVo.getLabel().toLowerCase().contains(keyword)) {
+                    iterator.remove();
+                }
+            }
+        }
+        checkCiAuth(ciList);
+        for (CiVo ciVo : ciList) {
+            CiCatalogNodeVo ciNode = new CiCatalogNodeVo(ciVo);
+            if (ciVo.getCatalogId() == null) {
+                noCatalogCiNodeList.add(ciNode);
+                continue;
+            }
+            CiCatalogNodeVo node = id2NodeMap.get(ciVo.getCatalogId());
+            if (node == null) {
+                noCatalogCiNodeList.add(ciNode);
+                continue;
+            }
+            matchCiCatalogNodeList.add(node);
+            hasCatalogCiNodeList.add(ciNode);
+        }
+
+        for (CiCatalogNodeVo node : allNodeList) {
+            for (CiCatalogNodeVo matchCiCatalogNode : matchCiCatalogNodeList) {
+                if (node.getLft() <= matchCiCatalogNode.getLft() && node.getRht() >= matchCiCatalogNode.getRht()) {
                     catalogList.add(node);
                 }
-            }
-            for (CiVo ciVo : ciList) {
-                CiCatalogNodeVo ciNode = new CiCatalogNodeVo(ciVo);
-                if (ciVo.getCatalogId() == null) {
-                    noCatalogCiNodeList.add(ciNode);
-                    continue;
-                }
-                CiCatalogNodeVo node = id2NodeMap.get(ciVo.getCatalogId());
-                if (node == null) {
-                    noCatalogCiNodeList.add(ciNode);
-                    continue;
-                }
-                ciNodeList.add(ciNode);
             }
         }
 
         if (CollectionUtils.isNotEmpty(catalogList)) {
             for (CiCatalogNodeVo node : catalogList) {
+                node.setType(CiCatalogNodeVo.CATALOG);
                 CiCatalogNodeVo parent = id2NodeMap.get(node.getParentId());
                 if (parent != null) {
                     parent.addChild(node);
                 }
             }
-            for (CiCatalogNodeVo node : ciNodeList) {
+            for (CiCatalogNodeVo node : hasCatalogCiNodeList) {
                 CiCatalogNodeVo parent = id2NodeMap.get(node.getParentId());
                 if (parent != null) {
                     parent.addChild(node);
                 }
             }
+            CiCatalogNodeVo rootNode = allNodeList.get(0);
             for (CiCatalogNodeVo node : noCatalogCiNodeList) {
                 rootNode.addChild(node);
             }
@@ -143,4 +137,31 @@ public class ListCiCatalogAndCiForTreeApi extends PrivateApiComponentBase {
         return "cmdb/cicatalogandci/listtree";
     }
 
+    private void checkCiAuth(List<CiVo> ciList) {
+        //如果没有管理权限则需要检查每个模型的权限
+        if (!AuthActionChecker.check(CI_MODIFY.class, CIENTITY_MODIFY.class)) {
+            Map<Long, List<CiAuthVo>> ciId2CiAuthListMap = new HashMap<>();
+            List<Long> ciIdList = ciList.stream().map(CiVo::getId).collect(Collectors.toList());
+            List<CiAuthVo> ciAuthList = ciAuthMapper.getCiAuthByCiIdList(ciIdList);
+            for (CiAuthVo ciAuth : ciAuthList) {
+                ciId2CiAuthListMap.computeIfAbsent(ciAuth.getCiId(), key -> new ArrayList<>()).add(ciAuth);
+            }
+            Iterator<CiVo> iterator = ciList.iterator();
+            while (iterator.hasNext()) {
+                CiVo ciVo = iterator.next();
+                List<CiAuthVo> ciAuths = ciId2CiAuthListMap.get(ciVo.getId());
+                if (CollectionUtils.isNotEmpty(ciAuths)) {
+                    if (!CiAuthChecker.hasPrivilege(ciAuths, CiAuthType.CIMANAGE, CiAuthType.CIENTITYUPDATE, CiAuthType.CIENTITYDELETE, CiAuthType.TRANSACTIONMANAGE, CiAuthType.CIENTITYQUERY)) {
+                        if (!CiAuthChecker.isCiInGroup(ciVo.getId(), GroupType.READONLY, GroupType.MAINTAIN)) {
+                            iterator.remove();
+                        }
+                    }
+                } else {
+                    if (!CiAuthChecker.isCiInGroup(ciVo.getId(), GroupType.READONLY, GroupType.MAINTAIN)) {
+                        iterator.remove();
+                    }
+                }
+            }
+        }
+    }
 }
