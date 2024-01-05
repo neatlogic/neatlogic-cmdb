450033f3262a33d75db02a64e8dd3bf643ed8947
chenqw <zenkiray@live.com>
2024-01-05 22:07:55 +0800

    adjust
    Signed-off-by: chenqw <zenkiray@live.com>

--- src/main/java/neatlogic/module/cmdb/api/customview/SaveCustomViewApi.java
+++ src/main/java/neatlogic/module/cmdb/api/customview/SaveCustomViewApi.java
@@ -16,18 +16,15 @@
 
 package neatlogic.module.cmdb.api.customview;
 
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.asynchronization.threadlocal.UserContext;
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.auth.core.AuthActionChecker;
 import neatlogic.framework.cmdb.auth.label.CMDB_BASE;
 import neatlogic.framework.cmdb.auth.label.CUSTOMVIEW_MODIFY;
-import neatlogic.framework.cmdb.dto.customview.*;
+import neatlogic.framework.cmdb.dto.customview.CustomViewVo;
 import neatlogic.framework.cmdb.enums.customview.CustomViewType;
-import neatlogic.framework.cmdb.enums.customview.RelType;
 import neatlogic.framework.cmdb.exception.customview.CustomViewCiNotFoundException;
-import neatlogic.framework.cmdb.exception.customview.CustomViewEmptyException;
 import neatlogic.framework.cmdb.exception.customview.CustomViewNameIsExistsException;
 import neatlogic.framework.cmdb.exception.customview.CustomViewPrivilegeSaveException;
 import neatlogic.framework.common.constvalue.ApiParamType;
@@ -38,16 +35,11 @@ import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
 import neatlogic.module.cmdb.dao.mapper.customview.CustomViewMapper;
 import neatlogic.module.cmdb.service.ci.CiAuthChecker;
 import neatlogic.module.cmdb.service.customview.CustomViewService;
-import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Service
 @AuthAction(action = CMDB_BASE.class)
@@ -76,7 +68,14 @@ public class SaveCustomViewApi extends PrivateApiComponentBase {
         return null;
     }
 
-    @Input({@Param(name = "id", type = ApiParamType.LONG, desc = "视图id，不提供代表新增"), @Param(name = "ciId", type = ApiParamType.LONG, desc = "模型id，保存场景视图时需要提供"), @Param(name = "name", type = ApiParamType.STRING, isRequired = true, xss = true, maxLength = 50, desc = "名称"), @Param(name = "icon", type = ApiParamType.STRING, desc = "图标"), @Param(name = "isActive", type = ApiParamType.INTEGER, isRequired = true, desc = "是否激活"), @Param(name = "type", type = ApiParamType.ENUM, member = CustomViewType.class, isRequired = true, desc = "类型"), @Param(name = "authList", type = ApiParamType.JSONARRAY, desc = "授权列表", help = "仅对公共视图生效"), @Param(name = "config", type = ApiParamType.JSONOBJECT, isRequired = true, desc = "拓扑图配置")})
+    @Input({@Param(name = "id", type = ApiParamType.LONG, desc = "视图id，不提供代表新增"),
+            @Param(name = "ciId", type = ApiParamType.LONG, desc = "模型id，保存场景视图时需要提供"),
+            @Param(name = "name", type = ApiParamType.STRING, isRequired = true, xss = true, maxLength = 50, desc = "名称"),
+            @Param(name = "icon", type = ApiParamType.STRING, desc = "图标"),
+            @Param(name = "isActive", type = ApiParamType.INTEGER, isRequired = true, desc = "是否激活"),
+            @Param(name = "type", type = ApiParamType.ENUM, member = CustomViewType.class, isRequired = true, desc = "类型"),
+            @Param(name = "authList", type = ApiParamType.JSONARRAY, desc = "授权列表", help = "仅对公共视图生效"),
+            @Param(name = "config", type = ApiParamType.JSONOBJECT, isRequired = true, desc = "拓扑图配置")})
     @Output({@Param(name = "Return", type = ApiParamType.LONG, desc = "视图id")})
     @Description(desc = "保存自定义视图接口")
     @Override
