940d7f590bf7884d9f3e2ba01793feefe6ba6e23
linbangquan <1437892690@qq.com>
2024-01-09 17:29:04 +0800

    [功能] 后端-CMDB模型分派器
    关联 #[1065220494032896]后端-CMDB模型分派器 http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/1065220494032896

--- src/main/java/neatlogic/module/cmdb/api/customview/GetCustomViewAttrApi.java
+++ src/main/java/neatlogic/module/cmdb/api/customview/GetCustomViewAttrApi.java
@@ -16,16 +16,19 @@
 
 package neatlogic.module.cmdb.api.customview;
 
+import com.alibaba.fastjson.JSONArray;
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.cmdb.dto.customview.CustomViewAttrVo;
 import neatlogic.framework.cmdb.dto.customview.CustomViewConstAttrVo;
 import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.exception.type.ParamNotExistsException;
 import neatlogic.framework.restful.annotation.*;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
 import neatlogic.framework.cmdb.auth.label.CMDB_BASE;
 import neatlogic.module.cmdb.dao.mapper.customview.CustomViewMapper;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -49,12 +52,30 @@ public class GetCustomViewAttrApi extends PrivateApiComponentBase {
         return null;
     }
 
-    @Input({@Param(name = "id", type = ApiParamType.LONG, desc = "视图id", isRequired = true), @Param(name = "isHidden", type = ApiParamType.INTEGER, desc = "属性是否隐藏，1是，0否")})
-    @Output({@Param(name = "attrList", explode = CustomViewAttrVo[].class), @Param(name = "constAttrList", explode = CustomViewConstAttrVo[].class)})
+    @Input({
+            @Param(name = "id", type = ApiParamType.LONG, desc = "视图id"),
+            @Param(name = "isHidden", type = ApiParamType.INTEGER, desc = "属性是否隐藏，1是，0否"),
+            @Param(name = "defaultValue", type = ApiParamType.JSONARRAY, desc = "common.defaultvalue")
+    })
+    @Output({
+            @Param(name = "attrList", explode = CustomViewAttrVo[].class),
+            @Param(name = "constAttrList", explode = CustomViewConstAttrVo[].class)
+    })
     @Description(desc = "获取自定义视图属性列表接口")
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
+        JSONArray defaultValue = paramObj.getJSONArray("defaultValue");
+        if (CollectionUtils.isNotEmpty(defaultValue)) {
+            List<String> uuidList = defaultValue.toJavaList(String.class);
+            List<CustomViewAttrVo> attrList = customViewMapper.getCustomViewAttrByUuidList(uuidList);
+            JSONObject returnObj = new JSONObject();
+            returnObj.put("attrList", attrList);
+            return returnObj;
+        }
         Long id = paramObj.getLong("id");
+        if (id == null) {
+            throw new ParamNotExistsException("id");
+        }
         Integer isHidden = paramObj.getInteger("isHidden");
         CustomViewConstAttrVo customViewConstAttrVo = new CustomViewConstAttrVo(id);
         CustomViewAttrVo customViewAttrVo = new CustomViewAttrVo(id);
