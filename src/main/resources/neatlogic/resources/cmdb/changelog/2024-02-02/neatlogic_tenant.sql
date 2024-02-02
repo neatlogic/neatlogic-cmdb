5f056914d170a9d55ae433f77bd996dcba111179
chenqw <zenkiray@live.com>
2024-02-02 18:06:12 +0800

    -[功能]增加采集对象和模型的映射关系配置功能
    Signed-off-by: chenqw <zenkiray@live.com>

--- src/main/resources/neatlogic/resources/cmdb/changelog/2024-02-02/neatlogic_tenant.sql
+++ src/main/resources/neatlogic/resources/cmdb/changelog/2024-02-02/neatlogic_tenant.sql
@@ -1,6 +1,9 @@
-CREATE TABLE `cmdb_sync_objtype` (
-                                     `obj_category` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT '对象大类',
-                                     `obj_type` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT '对象小类',
-                                     `ci_id` bigint DEFAULT NULL COMMENT '模型id',
-                                     PRIMARY KEY (`obj_category`,`obj_type`)
+CREATE TABLE IF NOT EXISTS `cmdb_sync_objtype` (
+                                                   `id` bigint NOT NULL,
+                                                   `obj_category` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT '对象大类',
+                                                   `obj_type` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT '对象小类',
+                                                   `ci_id` bigint DEFAULT NULL COMMENT '模型id',
+                                                   PRIMARY KEY (`id`) USING BTREE,
+                                                   UNIQUE KEY `uk_obj` (`obj_category`,`obj_type`) USING BTREE,
+                                                   UNIQUE KEY `uk_ciid` (`ci_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
--- src/main/resources/neatlogic/resources/cmdb/sqlscript/ddl.sql
+++ src/main/resources/neatlogic/resources/cmdb/sqlscript/ddl.sql
@@ -1015,3 +1015,16 @@ CREATE TABLE IF NOT EXISTS `cmdb_topo_template` (
                                       KEY `idx_ci` (`ci_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- ----------------------------
+-- Table structure for cmdb_sync_objtype
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `cmdb_sync_objtype` (
+                                     `id` bigint NOT NULL,
+                                     `obj_category` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT '对象大类',
+                                     `obj_type` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT '对象小类',
+                                     `ci_id` bigint DEFAULT NULL COMMENT '模型id',
+                                     PRIMARY KEY (`id`) USING BTREE,
+                                     UNIQUE KEY `uk_obj` (`obj_category`,`obj_type`) USING BTREE,
+                                     UNIQUE KEY `uk_ciid` (`ci_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
