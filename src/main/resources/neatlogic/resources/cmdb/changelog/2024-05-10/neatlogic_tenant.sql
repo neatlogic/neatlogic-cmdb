638f39a3e6b296e6e6a174cdbbf37a10e7d509c7
chenqw <zenkiray@live.com>
2024-05-10 16:47:04 +0800

    删除补全
    Signed-off-by: chenqw <zenkiray@live.com>

--- src/main/resources/neatlogic/resources/cmdb/changelog/2024-05-10/neatlogic_tenant.sql
+++ src/main/resources/neatlogic/resources/cmdb/changelog/2024-05-10/neatlogic_tenant.sql
@@ -2,3 +2,7 @@ ALTER TABLE `cmdb_sync_mapping`
     MODIFY COLUMN `action` enum ('insert','replace','delete','update','merge') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT 'insert' COMMENT '动作' AFTER `field`;
 ALTER TABLE `cmdb_cientity_illegal`
     ADD INDEX `idx_cientity_id` (`cientity_id`) USING BTREE;
+ALTER TABLE `cmdb_cientity_inspect`
+    ADD INDEX `idx_cientity_id`(`ci_entity_id`) USING BTREE;
+ALTER TABLE `cmdb_cientity_globalattritem`
+    ADD INDEX `idx_cientity_id`(`cientity_id`) USING BTREE;
