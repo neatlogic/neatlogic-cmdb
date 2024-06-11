5db118b2beca2de03716079c9dddcde74fc756a6
chenqw <zenkiray@live.com>
2024-06-11 21:17:30 +0800

    -[修复]CMDB上传模板无法下载问题
    Signed-off-by: chenqw <zenkiray@live.com>

---
+++ src/main/java/neatlogic/module/cmdb/file/CiEntityImportFileHandler.java
@@ -0,0 +1,61 @@
+/*Copyright (C) 2024  深圳极向量科技有限公司 All Rights Reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
+package neatlogic.module.cmdb.file;
+
+import com.alibaba.fastjson.JSONObject;
+import neatlogic.framework.file.core.FileTypeHandlerBase;
+import neatlogic.framework.file.dto.FileVo;
+import neatlogic.module.cmdb.dao.mapper.batchimport.ImportMapper;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component
+public class CiEntityImportFileHandler extends FileTypeHandlerBase {
+    @Resource
+    private ImportMapper importMapper;
+
+    @Override
+    public boolean valid(String userUuid, FileVo fileVo, JSONObject jsonObj) {
+        boolean isValid = false;
+        if (fileVo != null && StringUtils.isNotBlank(userUuid)) {
+            isValid = fileVo.getUserUuid().equals(userUuid);
+        }
+        return isValid;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "配置项上传附件";
+    }
+
+    @Override
+    protected boolean myDeleteFile(FileVo fileVo, JSONObject paramObj) {
+        importMapper.deleteCmdbImportFile(fileVo.getId());
+        return true;
+    }
+
+    @Override
+    public void afterUpload(FileVo fileVo, JSONObject jsonObj) {
+    }
+
+    @Override
+    public String getName() {
+        return "CIENTITYIMPORT";
+    }
+
+}
