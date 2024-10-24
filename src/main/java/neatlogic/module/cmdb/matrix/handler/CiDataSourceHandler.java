bb40e15a76fb89c2e8ce062f85985b3907974235
1437892690@qq.com <1437892690@qq.com>
2024-10-24 11:23:58 +0800

    [功能] 表单配置数据转换
    关联 #[1252134048202752]表单配置数据转换 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/1252134048202752

--- src/main/java/neatlogic/module/cmdb/matrix/handler/CiDataSourceHandler.java
+++ src/main/java/neatlogic/module/cmdb/matrix/handler/CiDataSourceHandler.java
@@ -259,14 +259,15 @@ public class CiDataSourceHandler extends MatrixDataSourceHandlerBase {
             if (CollectionUtils.isNotEmpty(showAttributeList)) {
                 for (int i = 0; i < showAttributeList.size(); i++) {
                     JSONObject showAttributeObj = showAttributeList.getJSONObject(i);
-                    if (MapUtils.isEmpty(showAttributeObj)) {
-                        continue;
+                    if (MapUtils.isNotEmpty(showAttributeObj)) {
+                        String label = showAttributeObj.getString("label");
+                        if (StringUtils.isNotBlank(label)) {
+                            JSONObject attributeMappingObj = new JSONObject();
+                            attributeMappingObj.put("label", label);
+                            attributeMappingObj.put("uniqueIdentifier", "");
+                            attributeMappingList.add(attributeMappingObj);
+                        }
                     }
-                    String label = showAttributeObj.getString("label");
-                    JSONObject attributeMappingObj = new JSONObject();
-                    attributeMappingObj.put("label", label);
-                    attributeMappingObj.put("uniqueIdentifier", "");
-                    attributeMappingList.add(attributeMappingObj);
                 }
             }
             config.put("attributeMappingList", attributeMappingList);
@@ -1140,17 +1141,16 @@ public class CiDataSourceHandler extends MatrixDataSourceHandlerBase {
         if (CollectionUtils.isNotEmpty(showAttributeList)) {
             for (int i = 0; i < showAttributeList.size(); i++) {
                 JSONObject showAttributeObj = showAttributeList.getJSONObject(i);
-                if (MapUtils.isEmpty(showAttributeObj)) {
-                    continue;
-                }
-                String label = showAttributeObj.getString("label");
-                if (StringUtils.isNotBlank(label)) {
-                    if (label.startsWith("attr_")) {
-                        attrIdList.add(Long.valueOf(label.substring(5)));
-                    } else if (label.startsWith("relfrom_")) {
-                        relIdList.add(Long.valueOf(label.substring(8)));
-                    } else if (label.startsWith("relto_")) {
-                        relIdList.add(Long.valueOf(label.substring(6)));
+                if (MapUtils.isNotEmpty(showAttributeObj)) {
+                    String label = showAttributeObj.getString("label");
+                    if (StringUtils.isNotBlank(label)) {
+                        if (label.startsWith("attr_")) {
+                            attrIdList.add(Long.valueOf(label.substring(5)));
+                        } else if (label.startsWith("relfrom_")) {
+                            relIdList.add(Long.valueOf(label.substring(8)));
+                        } else if (label.startsWith("relto_")) {
+                            relIdList.add(Long.valueOf(label.substring(6)));
+                        }
                     }
                 }
             }
