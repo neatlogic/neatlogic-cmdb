96fab1465cc2e1c3c50f8e696ade1cb060e7bab9
chenqw <gdnpc@163.com>
2025-02-27 15:33:05 +0800

    -[功能]保存issue接口自定义属性支持AttrName
    Signed-off-by: chenqw <gdnpc@163.com>

---
+++ src/main/java/neatlogic/module/cmdb/api/TestApi.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2024  深圳极向量科技有限公司 All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package neatlogic.module.cmdb.api;
+
+import com.alibaba.fastjson.JSONObject;
+import neatlogic.framework.auth.core.AuthAction;
+import neatlogic.framework.cmdb.auth.label.CMDB_BASE;
+import neatlogic.framework.mq.core.ITopic;
+import neatlogic.framework.mq.core.TopicFactory;
+import neatlogic.framework.restful.annotation.OperationType;
+import neatlogic.framework.restful.constvalue.OperationTypeEnum;
+import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
+import org.springframework.stereotype.Service;
+
+@Service
+@AuthAction(action = CMDB_BASE.class)
+@OperationType(type = OperationTypeEnum.OPERATE)
+public class TestApi extends PrivateApiComponentBase {
+
+
+    @Override
+    public Object myDoService(JSONObject paramObj) throws Exception {
+        ITopic<String> topic2 = TopicFactory.getTopic("cmdb_cientity_update");
+        if (topic2 != null) {
+            topic2.send("hahaha");
+        }
+        return null;
+    }
+
+    @Override
+    public String getToken() {
+        return "cmdb/test";
+    }
+
+    @Override
+    public String getName() {
+        return "测试MQ";
+    }
+}
