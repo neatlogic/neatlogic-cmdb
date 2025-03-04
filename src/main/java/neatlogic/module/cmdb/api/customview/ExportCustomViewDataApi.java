c34b5a7def5e4628afc13a3a1f13df1f6d4638c6
chenqw <gdnpc@163.com>
2025-03-04 11:32:04 +0800

    -[修改]限制配置项导出的并发次数
    Signed-off-by: chenqw <gdnpc@163.com>

--- src/main/java/neatlogic/module/cmdb/api/customview/ExportCustomViewDataApi.java
+++ src/main/java/neatlogic/module/cmdb/api/customview/ExportCustomViewDataApi.java
@@ -20,6 +20,7 @@ import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.cmdb.auth.label.CMDB_BASE;
 import neatlogic.framework.cmdb.dto.customview.*;
+import neatlogic.framework.cmdb.exception.cientity.CiEntityIsExportingException;
 import neatlogic.framework.cmdb.exception.customview.CustomViewNotFoundException;
 import neatlogic.framework.common.constvalue.ApiParamType;
 import neatlogic.framework.restful.annotation.*;
@@ -49,12 +50,14 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Service
 @AuthAction(action = CMDB_BASE.class)
 @OperationType(type = OperationTypeEnum.SEARCH)
 public class ExportCustomViewDataApi extends PrivateBinaryStreamApiComponentBase {
     private static final Logger logger = LoggerFactory.getLogger(ExportCustomViewDataApi.class);
+    private final ReentrantLock exportLock = new ReentrantLock();
     @Resource
     private CustomViewDataService customViewDataService;
 
@@ -86,102 +89,111 @@ public class ExportCustomViewDataApi extends PrivateBinaryStreamApiComponentBase
     @Description(desc = "查询自定义视图数据")
     @Override
     public Object myDoService(JSONObject paramObj, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        CustomViewConditionVo customViewConditionVo = JSON.toJavaObject(paramObj, CustomViewConditionVo.class);
-        Long customViewId = paramObj.getLong("id");
-        CustomViewVo customViewVo = customViewMapper.getCustomViewById(customViewId);
-        if (customViewVo == null) {
-            throw new CustomViewNotFoundException(customViewId);
-        }
-        CustomViewAttrVo pCustomViewAttrVo = new CustomViewAttrVo();
-        pCustomViewAttrVo.setCustomViewId(customViewId);
-        pCustomViewAttrVo.setIsHidden(0);
-        CustomViewConstAttrVo pCustomViewConstAttrVo = new CustomViewConstAttrVo();
-        pCustomViewConstAttrVo.setCustomViewId(customViewId);
-        pCustomViewConstAttrVo.setIsHidden(0);
-        CustomViewGlobalAttrVo pCustomViewGlobalAttrVo = new CustomViewGlobalAttrVo();
-        pCustomViewGlobalAttrVo.setCustomViewId(customViewId);
-        pCustomViewGlobalAttrVo.setIsHidden(0);
-        List<CustomViewAttrVo> attrList = customViewMapper.getCustomViewAttrByCustomViewId(pCustomViewAttrVo);
-        List<CustomViewConstAttrVo> constAttrList = customViewMapper.getCustomViewConstAttrByCustomViewId(pCustomViewConstAttrVo);
-        List<CustomViewGlobalAttrVo> globalAttrList = customViewMapper.getCustomViewGlobalAttrByCustomViewId(pCustomViewGlobalAttrVo);
-        List<JSONObject> attrsList = new ArrayList<>();
-        if (CollectionUtils.isNotEmpty(attrList)) {
-            for (CustomViewAttrVo attrVo : attrList) {
-                JSONObject dataObj = new JSONObject();
-                dataObj.put("alias", attrVo.getAlias());
-                dataObj.put("uuid", attrVo.getUuid());
-                dataObj.put("sort", attrVo.getSort());
-                attrsList.add(dataObj);
+        try {
+            if (!exportLock.tryLock()) {
+                throw new CiEntityIsExportingException();
             }
-        }
-        if (CollectionUtils.isNotEmpty(constAttrList)) {
-            for (CustomViewConstAttrVo attrVo : constAttrList) {
-                JSONObject dataObj = new JSONObject();
-                dataObj.put("alias", attrVo.getAlias());
-                dataObj.put("uuid", attrVo.getUuid());
-                dataObj.put("sort", attrVo.getSort());
-                attrsList.add(dataObj);
+            CustomViewConditionVo customViewConditionVo = JSON.toJavaObject(paramObj, CustomViewConditionVo.class);
+            Long customViewId = paramObj.getLong("id");
+            CustomViewVo customViewVo = customViewMapper.getCustomViewById(customViewId);
+            if (customViewVo == null) {
+                throw new CustomViewNotFoundException(customViewId);
             }
-        }
-        if (CollectionUtils.isNotEmpty(globalAttrList)) {
-            for (CustomViewGlobalAttrVo attrVo : globalAttrList) {
-                JSONObject dataObj = new JSONObject();
-                dataObj.put("alias", attrVo.getAlias());
-                dataObj.put("uuid", attrVo.getUuid());
-                dataObj.put("sort", attrVo.getSort());
-                attrsList.add(dataObj);
-            }
-        }
-        attrsList.sort(Comparator.comparing(o -> o.getInteger("sort")));
-        List<String> headerList = new ArrayList<>();
-        List<String> columnList = new ArrayList<>();
-        for (JSONObject attr : attrsList) {
-            headerList.add(attr.getString("alias"));
-            columnList.add(attr.getString("uuid"));
-        }
-
-        ExcelBuilder builder = new ExcelBuilder(SXSSFWorkbook.class);
-        SheetBuilder sheetBuilder = builder
-                .withBorderColor(HSSFColor.HSSFColorPredefined.GREY_40_PERCENT)
-                .withHeadFontColor(HSSFColor.HSSFColorPredefined.WHITE)
-                .withHeadBgColor(HSSFColor.HSSFColorPredefined.DARK_BLUE)
-                .withColumnWidth(30).addSheet("数据")
-                .withHeaderList(headerList)
-                .withColumnList(columnList);
-        Workbook workbook = builder.build();
-
-        customViewConditionVo.setCustomViewId(customViewId);
-        customViewConditionVo.setPageSize(100);
-        customViewConditionVo.setCurrentPage(1);
-        List<Map<String, Object>> dataList = customViewDataService.searchCustomViewData(customViewConditionVo);
-        String fileNameEncode = customViewVo.getName() + ".xlsx";
-        if (request.getHeader("User-Agent").toLowerCase().contains("msie") || request.getHeader("User-Agent").contains("Gecko")) {
-            fileNameEncode = URLEncoder.encode(fileNameEncode, "UTF-8");// IE浏览器
-        } else {
-            fileNameEncode = new String(fileNameEncode.replace(" ", "").getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
-        }
-        response.setContentType("application/vnd.ms-excel;charset=utf-8");
-        response.setHeader("Content-Disposition", " attachment; filename=\"" + fileNameEncode + "\"");
-        try (OutputStream os = response.getOutputStream()) {
-            while (CollectionUtils.isNotEmpty(dataList)) {
-                //由于展示页面的特殊性，查询sql用的是pageSizePlus，所以要去掉最后一条数据
-                for (int i = 0; i < Math.min(customViewConditionVo.getPageSize(), dataList.size()); i++) {
-                    sheetBuilder.addData(dataList.get(i));
-                    ((SXSSFSheet) workbook.getSheetAt(0)).flushRows();
+            CustomViewAttrVo pCustomViewAttrVo = new CustomViewAttrVo();
+            pCustomViewAttrVo.setCustomViewId(customViewId);
+            pCustomViewAttrVo.setIsHidden(0);
+            CustomViewConstAttrVo pCustomViewConstAttrVo = new CustomViewConstAttrVo();
+            pCustomViewConstAttrVo.setCustomViewId(customViewId);
+            pCustomViewConstAttrVo.setIsHidden(0);
+            CustomViewGlobalAttrVo pCustomViewGlobalAttrVo = new CustomViewGlobalAttrVo();
+            pCustomViewGlobalAttrVo.setCustomViewId(customViewId);
+            pCustomViewGlobalAttrVo.setIsHidden(0);
+            List<CustomViewAttrVo> attrList = customViewMapper.getCustomViewAttrByCustomViewId(pCustomViewAttrVo);
+            List<CustomViewConstAttrVo> constAttrList = customViewMapper.getCustomViewConstAttrByCustomViewId(pCustomViewConstAttrVo);
+            List<CustomViewGlobalAttrVo> globalAttrList = customViewMapper.getCustomViewGlobalAttrByCustomViewId(pCustomViewGlobalAttrVo);
+            List<JSONObject> attrsList = new ArrayList<>();
+            if (CollectionUtils.isNotEmpty(attrList)) {
+                for (CustomViewAttrVo attrVo : attrList) {
+                    JSONObject dataObj = new JSONObject();
+                    dataObj.put("alias", attrVo.getAlias());
+                    dataObj.put("uuid", attrVo.getUuid());
+                    dataObj.put("sort", attrVo.getSort());
+                    attrsList.add(dataObj);
                 }
-                customViewConditionVo.setCurrentPage(customViewConditionVo.getCurrentPage() + 1);
-                dataList = customViewDataService.searchCustomViewData(customViewConditionVo);
             }
-            workbook.write(os);
-        } catch (IOException e) {
-            logger.error(e.getMessage(), e);
+            if (CollectionUtils.isNotEmpty(constAttrList)) {
+                for (CustomViewConstAttrVo attrVo : constAttrList) {
+                    JSONObject dataObj = new JSONObject();
+                    dataObj.put("alias", attrVo.getAlias());
+                    dataObj.put("uuid", attrVo.getUuid());
+                    dataObj.put("sort", attrVo.getSort());
+                    attrsList.add(dataObj);
+                }
+            }
+            if (CollectionUtils.isNotEmpty(globalAttrList)) {
+                for (CustomViewGlobalAttrVo attrVo : globalAttrList) {
+                    JSONObject dataObj = new JSONObject();
+                    dataObj.put("alias", attrVo.getAlias());
+                    dataObj.put("uuid", attrVo.getUuid());
+                    dataObj.put("sort", attrVo.getSort());
+                    attrsList.add(dataObj);
+                }
+            }
+            attrsList.sort(Comparator.comparing(o -> o.getInteger("sort")));
+            List<String> headerList = new ArrayList<>();
+            List<String> columnList = new ArrayList<>();
+            for (JSONObject attr : attrsList) {
+                headerList.add(attr.getString("alias"));
+                columnList.add(attr.getString("uuid"));
+            }
+
+            ExcelBuilder builder = new ExcelBuilder(SXSSFWorkbook.class);
+            SheetBuilder sheetBuilder = builder
+                    .withBorderColor(HSSFColor.HSSFColorPredefined.GREY_40_PERCENT)
+                    .withHeadFontColor(HSSFColor.HSSFColorPredefined.WHITE)
+                    .withHeadBgColor(HSSFColor.HSSFColorPredefined.DARK_BLUE)
+                    .withColumnWidth(30).addSheet("数据")
+                    .withHeaderList(headerList)
+                    .withColumnList(columnList);
+            Workbook workbook = builder.build();
+
+            customViewConditionVo.setCustomViewId(customViewId);
+            customViewConditionVo.setPageSize(100);
+            customViewConditionVo.setCurrentPage(1);
+            List<Map<String, Object>> dataList = customViewDataService.searchCustomViewData(customViewConditionVo);
+            String fileNameEncode = customViewVo.getName() + ".xlsx";
+            if (request.getHeader("User-Agent").toLowerCase().contains("msie") || request.getHeader("User-Agent").contains("Gecko")) {
+                fileNameEncode = URLEncoder.encode(fileNameEncode, "UTF-8");// IE浏览器
+            } else {
+                fileNameEncode = new String(fileNameEncode.replace(" ", "").getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
+            }
+            response.setContentType("application/vnd.ms-excel;charset=utf-8");
+            response.setHeader("Content-Disposition", " attachment; filename=\"" + fileNameEncode + "\"");
+            try (OutputStream os = response.getOutputStream()) {
+                while (CollectionUtils.isNotEmpty(dataList)) {
+                    //由于展示页面的特殊性，查询sql用的是pageSizePlus，所以要去掉最后一条数据
+                    for (int i = 0; i < Math.min(customViewConditionVo.getPageSize(), dataList.size()); i++) {
+                        sheetBuilder.addData(dataList.get(i));
+                        ((SXSSFSheet) workbook.getSheetAt(0)).flushRows();
+                    }
+                    customViewConditionVo.setCurrentPage(customViewConditionVo.getCurrentPage() + 1);
+                    dataList = customViewDataService.searchCustomViewData(customViewConditionVo);
+                }
+                workbook.write(os);
+            } catch (IOException e) {
+                logger.error(e.getMessage(), e);
+            } finally {
+                if (workbook != null) {
+                    ((SXSSFWorkbook) workbook).dispose(); // 清理内存缓存
+                    workbook.close();
+                }
+            }
+            return null;
         } finally {
-            if (workbook != null) {
-                ((SXSSFWorkbook) workbook).dispose(); // 清理内存缓存
-                workbook.close();
+            if (exportLock.isLocked() && exportLock.isHeldByCurrentThread()) {
+                exportLock.unlock();
             }
         }
-        return null;
     }
 
 }
