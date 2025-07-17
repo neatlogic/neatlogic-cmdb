5ac16eddd0e4b32e3ba8ada4d7f3c4e3867166da
chenqw <gdnpc@163.com>
2025-07-17 16:54:08 +0800

    -[修改]去掉危险的查询方法：getCiEntityByIdList
    Signed-off-by: chenqw <gdnpc@163.com>

--- src/main/java/neatlogic/module/cmdb/service/cientity/CiEntityServiceImpl.java
+++ src/main/java/neatlogic/module/cmdb/service/cientity/CiEntityServiceImpl.java
@@ -298,15 +298,6 @@ public class CiEntityServiceImpl implements CiEntityService, ICiEntityCrossoverS
         return getCiEntityByIdLite(ciEntityVo.getCiId(), ciEntityVo.getId(), false, ciEntityVo.isLimitRelEntity(), ciEntityVo.isLimitAttrEntity(), ciEntityVo.getGlobalAttrIdList(), ciEntityVo.getAttrIdList(), ciEntityVo.getRelIdList(), null, null);
     }
 
-    @Override
-    public List<CiEntityVo> getCiEntityByIdList(Long ciId, List<Long> ciEntityIdList) {
-        CiEntityVo ciEntityVo = new CiEntityVo();
-        ciEntityVo.setCiId(ciId);
-        ciEntityVo.setIdList(ciEntityIdList);
-        ciEntityVo.setLimitAttrEntity(true);
-        ciEntityVo.setLimitRelEntity(true);
-        return getCiEntityByIdList(ciEntityVo);
-    }
 
     @Override
     public List<CiEntityVo> getCiEntityNameByIdList(Long ciId, List<Long> ciEntityIdList) {
