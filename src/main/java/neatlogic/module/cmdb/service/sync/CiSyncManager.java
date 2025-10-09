d7d18cc1052e8042bee5dedf37eb258ef4b27db2
chenqw <gdnpc@163.com>
2025-10-09 19:09:19 +0800

    -[修复]下拉多选作为唯一属性时的判断异常
    Signed-off-by: chenqw <gdnpc@163.com>

--- src/main/java/neatlogic/module/cmdb/service/sync/CiSyncManager.java
+++ src/main/java/neatlogic/module/cmdb/service/sync/CiSyncManager.java
@@ -324,7 +324,6 @@ public class CiSyncManager {
                     filterVo.setType(attr.getType());
                     filterVo.setAttrId(syncMappingVo.getAttrId());
                     filterVo.setExpression(SearchExpression.EQ.getExpression());
-                    filterVo.setIsStrict(true);
                     String v = dataObj.getString(syncMappingVo.getField(parentKey));
                     if (Boolean.FALSE.equals(attr.isNeedTargetCi())) {
                         if (StringUtils.isNotBlank(v)) {
