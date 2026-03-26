7f24571b928e825338abcdf4d74f72ae5285c9e8
1437892690@qq.com <1437892690@qq.com>
2026-03-26 16:24:40 +0800

    [修复] 资产清单-用户有资源中心和账号管理权限，但是没有资产数据权限，可以越过数据权限修改帐号
    关联 #[1647750790512645]资产清单-用户有资源中心和账号管理权限，但是没有资产数据权限，可以越过数据权限修改帐号 http://192.168.0.96:8090/demo/rdm.html#/bug-detail/939050947543040/939050947543057/1647750790512645

--- src/main/java/neatlogic/module/cmdb/dependency/ResourceAccountDependencyHandler.java
+++ src/main/java/neatlogic/module/cmdb/dependency/ResourceAccountDependencyHandler.java
@@ -8,8 +8,8 @@ import neatlogic.framework.dependency.core.CustomDependencyHandlerBase;
 import neatlogic.framework.dependency.core.IFromType;
 import neatlogic.framework.dependency.dto.DependencyInfoVo;
 import neatlogic.module.cmdb.dao.mapper.resourcecenter.ResourceAccountMapper;
-import neatlogic.module.cmdb.dao.mapper.resourcecenter.ResourceMapper;
 import neatlogic.module.cmdb.service.resourcecenter.resource.IResourceCenterResourceService;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -73,7 +73,7 @@ public class ResourceAccountDependencyHandler extends CustomDependencyHandlerBas
     public List<DependencyInfoVo> getDependencyList(Object from, int startNum, int pageSize) {
         List<DependencyInfoVo> resultList = new ArrayList<>();
         List<Long> resourceIdList = resourceAccountMapper.getResourceIdListByAccountIdWithPage((Long) from, startNum, pageSize);
-        if (resourceIdList.size() > 0) {
+        if (CollectionUtils.isNotEmpty(resourceIdList)) {
             List<ResourceVo> resourceList = resourceCenterResourceService.getResourceListByIdList(resourceIdList);
             for (ResourceVo vo : resourceList) {
                 DependencyInfoVo dependencyInfoVo = parse(vo);
