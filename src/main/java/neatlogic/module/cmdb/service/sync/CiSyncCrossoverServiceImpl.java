5573c427d6b62f7f83655c05f94d3ca1ee779b57
lvzk <897706680@qq.com>
2026-05-12 14:21:53 +0800

    [功能]其它模块触发 cmdb 采集同步

---
+++ src/main/java/neatlogic/module/cmdb/service/sync/CiSyncCrossoverServiceImpl.java
@@ -0,0 +1,28 @@
+/*
+ *
+ * Copyright (C) 2025  TechSure Co., Ltd.  All Rights Reserved.
+ * This file is part of the NeatLogic software.
+ * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x – 2025.
+ * You may use this file only in compliance with the License.
+ * See the LICENSE file distributed with this work for the full license text.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ */
+
+package neatlogic.module.cmdb.service.sync;
+
+import com.alibaba.fastjson.JSONObject;
+import neatlogic.framework.cmdb.crossover.ICiSyncCrossoverService;
+import neatlogic.framework.cmdb.dto.sync.SyncCiCollectionVo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CiSyncCrossoverServiceImpl implements ICiSyncCrossoverService {
+    @Override
+    public void doSync(JSONObject dataObj, List<SyncCiCollectionVo> syncCiCollectionList) {
+        CiSyncManager.doSync(dataObj, syncCiCollectionList);
+    }
+}
