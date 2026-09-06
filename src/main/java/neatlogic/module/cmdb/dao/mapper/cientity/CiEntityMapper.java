a4fdc81bbcc5689ff1f91bd8c1b763ba22f88561
1437892690@qq.com <1437892690@qq.com>
2026-09-06 17:03:53 +0800

    [功能] 后端-机房管理
    关联 #[1729096900673543]后端-机房管理 http://192.168.0.96:8090/demo/rdm.html#/task-detail/939050947543040/939050947543050/1729096900673543

--- src/main/java/neatlogic/module/cmdb/dao/mapper/cientity/CiEntityMapper.java
+++ src/main/java/neatlogic/module/cmdb/dao/mapper/cientity/CiEntityMapper.java
@@ -226,10 +226,6 @@ public interface CiEntityMapper extends ICiEntityCrossoverMapper {
 
     Long getCiEntityIdByCiNameAndCiEntityName(@Param("ciName") String ciName, @Param("ciEntityName") String ciEntityName);
 
-//    Long getCiEntityIdByCiIdAndAttrIdAndAttrValue(@Param("ciId") Long ciId, @Param("attrId") Long attrId, @Param("attrValue") String attrValue);
-
-//    List<Map<String, Object>> getCiEntityIdAndAttrValueByCiIdAndAttrIdAndKeyword(@Param("ciId") Long ciId, @Param("attrId") Long id, @Param("keyword") String keyword);
-
     void deleteAttrEntityByFromCiEntityIdAndAttrId(@Param("fromCiEntityId") Long fromCiEntityId, @Param("attrId") Long attrId);
 
     void insertCiEntityExpiredTime(CiEntityVo ciEntityVo);
